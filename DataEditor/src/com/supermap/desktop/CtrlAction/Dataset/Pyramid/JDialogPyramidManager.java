60c0419602dda8df56b9e69e436b97e475dc9346
xiajt <xiajintao@supermap.com>
2016-01-22 17:38:21 +0800

    影像金子塔关闭时 dispose数据集选择器

--- DataEditor/src/com/supermap/desktop/CtrlAction/Dataset/Pyramid/JDialogPyramidManager.java
+++ DataEditor/src/com/supermap/desktop/CtrlAction/Dataset/Pyramid/JDialogPyramidManager.java
@@ -26,6 +26,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 
 /**
@@ -260,6 +262,13 @@ public class JDialogPyramidManager extends SmDialog {
 			@Override
 			public void tableChanged(TableModelEvent e) {
 				checkButtonStates();
+			}
+		});
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				JDialogPyramidManager.this.dispose();
+				JDialogPyramidManager.this.removeWindowListener(this);
 			}
 		});
 	}
