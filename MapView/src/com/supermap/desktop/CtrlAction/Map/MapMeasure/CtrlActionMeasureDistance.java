05b319ec0a2674e73edea6a3ec8991a6adb4bb7b
xiajt <xiajintao@supermap.com>
2016-02-18 15:30:04 +0800

    距离量算。

--- MapView/src/com/supermap/desktop/CtrlAction/Map/MapMeasure/CtrlActionMeasureDistance.java
+++ MapView/src/com/supermap/desktop/CtrlAction/Map/MapMeasure/CtrlActionMeasureDistance.java
@@ -5,13 +5,18 @@ import com.supermap.desktop.CtrlAction.Map.MapMeasure.Measure.MeasureDistance;
 import com.supermap.desktop.FormMap;
 import com.supermap.desktop.Interface.IBaseItem;
 import com.supermap.desktop.Interface.IForm;
+import com.supermap.desktop.Interface.IFormMap;
 import com.supermap.desktop.implement.CtrlAction;
+import com.supermap.ui.MapControl;
+
+import java.util.HashMap;
 
 /**
  * 距离量算
  */
 public class CtrlActionMeasureDistance extends CtrlAction {
-	private static MeasureDistance measureDistance = new MeasureDistance();
+
+	public static HashMap<MapControl, MeasureDistance> hashMap;
 
 	public CtrlActionMeasureDistance(IBaseItem caller, IForm formClass) {
 		super(caller, formClass);
@@ -19,8 +24,27 @@ public class CtrlActionMeasureDistance extends CtrlAction {
 
 	@Override
 	public void run() {
+		MeasureDistance measureDistance = getMeasureDistance();
+		if (measureDistance != null) {
+			measureDistance.startMeasure();
+		}
+	}
 
-		measureDistance.startMeasure();
+	private MeasureDistance getMeasureDistance() {
+		MeasureDistance result = null;
+		IForm activeForm = Application.getActiveApplication().getActiveForm();
+		if (activeForm instanceof IFormMap) {
+			MapControl mapControl = ((IFormMap) activeForm).getMapControl();
+			if (hashMap == null) {
+				hashMap = new HashMap<>();
+			}
+			result = hashMap.get(mapControl);
+			if (result == null) {
+				result = new MeasureDistance();
+				hashMap.put(mapControl, result);
+			}
+		}
+		return result;
 	}
 
 	@Override
