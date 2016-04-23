d251b2b5461a0b73893e27b60196e047b5c4383e
xiexiuji <xiexiuji@supermap.com>
2016-04-23 09:30:21 +0800

    鼠标查询栅格值移除鼠标事件。

--- SpatialAnalyst/src/com/supermap/desktop/Action/CtrlActionQueryGridValueByMouse.java
+++ SpatialAnalyst/src/com/supermap/desktop/Action/CtrlActionQueryGridValueByMouse.java
@@ -49,6 +49,9 @@ public class CtrlActionQueryGridValueByMouse extends CtrlAction {
 		formMap.showPopupMenu();
 		transparentBackground.setVisible(false);
 		TransparentBackground.queryGridMap.remove(mapControl.getMap().getName());
+		
+		mapControl.removeMouseListener(this.mouseAdapter);
+		mapControl.removeMouseMotionListener(this.mouseMotionListener);
 	}
 
 	KeyAdapter keyAdapter = new KeyAdapter() {
@@ -69,7 +72,7 @@ public class CtrlActionQueryGridValueByMouse extends CtrlAction {
 		}
 	};
 
-	MouseAdapter mapControlMouseMoved = new MouseAdapter() {
+	MouseAdapter mouseMotionListener = new MouseAdapter() {
 		@Override
 		public void mouseMoved(MouseEvent arg0) {
 			abstractMapcontrolMouseMoved(format, arg0);
@@ -117,12 +120,12 @@ public class CtrlActionQueryGridValueByMouse extends CtrlAction {
 
 	private void queryGridValue() {
 		mapControl.setAction(Action.SELECT);
-		mapControl.addMouseMotionListener(this.mapControlMouseMoved);
+		mapControl.addMouseMotionListener(this.mouseMotionListener);
 		// 不弹出菜单
 		formMap.dontShowPopupMenu();
 		mapControl.add(transparentBackground);
 		// 添加监听事件
-		mapControl.addMouseListener(mouseAdapter);
+		mapControl.addMouseListener(this.mouseAdapter);
 		mapControl.addKeyListener(keyAdapter);
 		mapControl.setLayout(null);
 		mapControl.getMap().addMapClosedListener(new MapClosedListener() {
