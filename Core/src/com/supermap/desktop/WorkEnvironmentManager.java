942a803b88c4997b5f339e1435343480704171d9
huchenpu <huchenpu@supermap.com>
2016-04-25 16:19:30 +0800

    解决Application被多次new的问题

--- Core/src/com/supermap/desktop/WorkEnvironmentManager.java
+++ Core/src/com/supermap/desktop/WorkEnvironmentManager.java
@@ -44,6 +44,11 @@ public class WorkEnvironmentManager {
 						Element pluginElement = XmlUtilties.getRootNode(configFile);
 						if (pluginElement != null) {
 							PluginInfo pluginInfo = new PluginInfo(pluginElement);
+							if (pluginInfo.getBundleName().indexOf("SuperMap.Desktop.LBSClient") != -1)
+							{
+								int n = 0;
+								int m = n;
+							}
 							if (pluginInfo.IsValid()
 									&& (pluginInfo.getBundleName().indexOf("SuperMap.Desktop.Frame") != -1 || pluginInfo.getBundleName().indexOf(
 											"SuperMap.Desktop.DataView") != -1)) {
