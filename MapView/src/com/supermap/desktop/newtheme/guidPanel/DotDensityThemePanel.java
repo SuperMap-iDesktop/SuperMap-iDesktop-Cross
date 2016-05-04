df26be2233628860bf4b536fa2438a5fdce6d3c5
xiexiuji <xiexiuji@supermap.com>
2016-05-04 15:03:59 +0800

    点密度专题图新建导航面板

---
+++ MapView/src/com/supermap/desktop/newtheme/guidPanel/DotDensityThemePanel.java
@@ -0,0 +1,82 @@
+package com.supermap.desktop.newtheme.guidPanel;
+
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.GroupLayout;
+import javax.swing.JPanel;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.border.LineBorder;
+
+import com.supermap.desktop.mapview.MapViewProperties;
+import com.supermap.desktop.ui.controls.InternalImageIconFactory;
+
+public class DotDensityThemePanel extends JPanel{
+	private static final long serialVersionUID = 1L;
+	private ThemeLabelDecorator dotDensityTheme;
+	private transient ThemeGuidDialog themeGuidDialog;
+	private transient LocalMouseListener mouseListener = new LocalMouseListener();
+
+	public DotDensityThemePanel(ThemeGuidDialog themeGuidDialog) {
+		this.themeGuidDialog = themeGuidDialog;
+		initComponents();
+		registListener();
+	}
+
+	/**
+	 * 界面布局入口
+	 */
+	private void initComponents() {
+		// @formatter:off
+		this.dotDensityTheme = new ThemeLabelDecorator(InternalImageIconFactory.THEMEGUIDE_DOTDENSITY, MapViewProperties.getString("String_ThemeDefaultItem"));
+		this.dotDensityTheme.selected(true);
+		this.setBackground(Color.WHITE);
+		this.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+				groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+								.addGap(16)
+								.addComponent(dotDensityTheme)
+								.addContainerGap(300, Short.MAX_VALUE))
+				);
+		groupLayout.setVerticalGroup(
+				groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+								.addGap(16)
+								.addComponent(dotDensityTheme)
+								.addContainerGap(200, Short.MAX_VALUE))
+				);
+		setLayout(groupLayout);
+		// @formatter:on
+	}
+
+	/**
+	 * 注册事件
+	 */
+	private void registListener() {
+		this.dotDensityTheme.addMouseListener(this.mouseListener);
+	}
+
+	/**
+	 * 注销事件
+	 */
+	public void unregistListener() {
+		this.dotDensityTheme.removeMouseListener(this.mouseListener);
+	}
+
+	class LocalMouseListener extends MouseAdapter {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if (e.getClickCount() == 2) {
+				// 点密度专题图
+				
+				themeGuidDialog.dispose();
+				unregistListener();
+			}
+		}
+
+	}
+}
