8da4d1f2b7b7636843309b937d9c5be4a2fc97aa
highsad <wuxiaobo@supermap.com>
2016-05-06 20:31:27 +0800

    完善擦除和 tip 提示。

--- MapEditor/src/com/supermap/desktop/geometryoperation/CtrlAction/CtrlActionErase.java
+++ MapEditor/src/com/supermap/desktop/geometryoperation/CtrlAction/CtrlActionErase.java
@@ -1,5 +1,7 @@
 package com.supermap.desktop.geometryoperation.CtrlAction;
 
+import javax.swing.Timer;
+
 import com.supermap.desktop.Interface.IBaseItem;
 import com.supermap.desktop.Interface.IForm;
 import com.supermap.desktop.geometryoperation.editor.EraseEditor;
@@ -8,6 +10,7 @@ import com.supermap.desktop.geometryoperation.editor.IEditor;
 public class CtrlActionErase extends CtrlActionEditorBase {
 
 	private EraseEditor editor = new EraseEditor();
+	Timer timer;
 
 	public CtrlActionErase(IBaseItem caller, IForm formClass) {
 		super(caller, formClass);
