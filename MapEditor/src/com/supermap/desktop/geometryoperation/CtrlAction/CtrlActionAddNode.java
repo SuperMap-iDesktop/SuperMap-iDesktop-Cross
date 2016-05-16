309d7fdb44429006a1b65054199fab5d9cdbd063
highsad <wuxiaobo@supermap.com>
2016-05-16 15:46:59 +0800

    新增添加节点、编辑节点功能配置。

---
+++ MapEditor/src/com/supermap/desktop/geometryoperation/CtrlAction/CtrlActionAddNode.java
@@ -0,0 +1,14 @@
+package com.supermap.desktop.geometryoperation.CtrlAction;
+
+import com.supermap.desktop.Interface.IBaseItem;
+import com.supermap.desktop.Interface.IForm;
+import com.supermap.desktop.implement.CtrlAction;
+
+public class CtrlActionAddNode extends CtrlAction{
+
+	public CtrlActionAddNode(IBaseItem caller, IForm formClass) {
+		super(caller, formClass);
+		// TODO Auto-generated constructor stub
+	}
+
+}
