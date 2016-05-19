12eedba1075a10dd27a4c226a5f609ddde8d1639
highsad <wuxiaobo@supermap.com>
2016-05-19 21:03:29 +0800

    对象类型转换的部分功能实现。

--- MapEditor/src/com/supermap/desktop/geometryoperation/control/JDialogGeometryConvert.java
+++ MapEditor/src/com/supermap/desktop/geometryoperation/control/JDialogGeometryConvert.java
@@ -1,5 +1,7 @@
 package com.supermap.desktop.geometryoperation.control;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
@@ -8,16 +10,24 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.SwingUtilities;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
+import com.supermap.data.Dataset;
+import com.supermap.data.DatasetType;
 import com.supermap.data.DatasetVector;
+import com.supermap.data.DatasetVectorInfo;
 import com.supermap.data.Datasource;
+import com.supermap.desktop.Application;
 import com.supermap.desktop.controls.ControlsProperties;
 import com.supermap.desktop.mapeditor.MapEditorProperties;
 import com.supermap.desktop.properties.CommonProperties;
+import com.supermap.desktop.ui.controls.DataCell;
 import com.supermap.desktop.ui.controls.DatasetComboBox;
 import com.supermap.desktop.ui.controls.DatasourceComboBox;
+import com.supermap.desktop.ui.controls.DialogResult;
 import com.supermap.desktop.ui.controls.SmDialog;
+import com.supermap.desktop.ui.controls.TextFields.ISmTextFieldLegit;
 import com.supermap.desktop.ui.controls.TextFields.SmTextFieldLegit;
 
 public class JDialogGeometryConvert extends SmDialog {
@@ -26,6 +36,7 @@ public class JDialogGeometryConvert extends SmDialog {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private static String DEFAULT_DATASET_NAME = "ConvertResult";
 
 	private JLabel labelDesDatasource;
 	private JLabel labelDesDataset;
@@ -37,6 +48,7 @@ public class JDialogGeometryConvert extends SmDialog {
 	private JButton buttonOK;
 	private JButton buttonCancel;
 
+	private DatasetType datasetType; // 目标数据集类型
 	private Datasource desDatasource;
 	private DatasetVector desDataset;
 	private String newDatasetName;
@@ -47,13 +59,109 @@ public class JDialogGeometryConvert extends SmDialog {
 
 		@Override
 		public void itemStateChanged(ItemEvent e) {
+			if (e.getSource() == JDialogGeometryConvert.this.comboBoxDatasource) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					comboBoxDesDatasourceSelectedChange();
+				}
+			} else if (e.getSource() == JDialogGeometryConvert.this.comboBoxDataset) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					comboBoxDesDatasetSelectedChange();
+				}
+			} else if (e.getSource() == JDialogGeometryConvert.this.checkBoxNewDataset) {
+				checkBoxNewDatasetCheckedChange();
+			} else if (e.getSource() == JDialogGeometryConvert.this.checkBoxRemoveSrc) {
+				checkBoxRemoveSrcCheckedChange();
+			}
+		}
+	};
+
+	private DocumentListener documentListener = new DocumentListener() {
+
+		@Override
+		public void removeUpdate(DocumentEvent e) {
+			textFieldNewDatasetTextChange();
+		}
+
+		@Override
+		public void insertUpdate(DocumentEvent e) {
+			textFieldNewDatasetTextChange();
+		}
+
+		@Override
+		public void changedUpdate(DocumentEvent e) {
 			// TODO Auto-generated method stub
 
 		}
 	};
 
-	public JDialogGeometryConvert(String title) {
+	private ActionListener actionListener = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == JDialogGeometryConvert.this.buttonOK) {
+				buttonOKClick();
+			} else if (e.getSource() == JDialogGeometryConvert.this.buttonCancel) {
+				buttonCancelClick();
+			}
+		}
+	};
+
+	public JDialogGeometryConvert(String title, DatasetType datasetType) {
+		this.datasetType = datasetType;
+		initializeDatas();
 		initializeComponents(title);
+		initializeComponentsValue();
+		registerEvents();
+	}
+
+	public Datasource getDesDatasource() {
+		return this.desDatasource;
+	}
+
+	public DatasetVector getDesDataset() {
+		return this.desDataset;
+	}
+
+	public boolean isNewDataset() {
+		return this.isNewDataset;
+	}
+
+	public boolean isRemoveSrc() {
+		return this.isRemoveSrc;
+	}
+
+	private void initializeDatas() {
+
+		// 目标数据源
+		if (Application.getActiveApplication().getActiveDatasources() != null && Application.getActiveApplication().getActiveDatasources().length > 0) {
+			this.desDatasource = Application.getActiveApplication().getActiveDatasources()[0];
+		}
+
+		// 目标数据集
+		initializeDesDataset();
+
+		// 其他
+		this.isNewDataset = false;
+		this.isRemoveSrc = false;
+
+		if (this.desDatasource != null) {
+			this.newDatasetName = this.desDatasource.getDatasets().getAvailableDatasetName(DEFAULT_DATASET_NAME);
+		}
+	}
+
+	private void initializeDesDataset() {
+		this.desDataset = null;
+
+		if (this.desDatasource != null) {
+			for (int i = 0; i < this.desDatasource.getDatasets().getCount(); i++) {
+				Dataset dataset = this.desDatasource.getDatasets().get(i);
+
+				if (dataset.getType() == this.datasetType) {
+					this.desDataset = (DatasetVector) dataset;
+					break;
+				}
+			}
+		}
 	}
 
 	private void initializeComponents(String title) {
@@ -61,12 +169,29 @@ public class JDialogGeometryConvert extends SmDialog {
 		this.labelDesDatasource = new JLabel(ControlsProperties.getString("String_Label_TargetDatasource"));
 		this.labelDesDataset = new JLabel(ControlsProperties.getString("String_Label_TargetDataset"));
 		this.comboBoxDatasource = new DatasourceComboBox();
-		this.comboBoxDataset = new DatasetComboBox();
+		this.comboBoxDataset = new DatasetComboBox(new Dataset[0]);
 		this.checkBoxNewDataset = new JCheckBox(ControlsProperties.getString("String_Label_NewDataset"));
 		this.textFieldNewDataset = new SmTextFieldLegit();
 		this.checkBoxRemoveSrc = new JCheckBox(MapEditorProperties.getString("String_RemoveSrcObj"));
 		this.buttonOK = new JButton(CommonProperties.getString(CommonProperties.OK));
 		this.buttonCancel = new JButton(CommonProperties.getString(CommonProperties.Cancel));
+
+		this.textFieldNewDataset.setSmTextFieldLegit(new ISmTextFieldLegit() {
+
+			@Override
+			public boolean isTextFieldValueLegit(String textFieldValue) {
+				if (JDialogGeometryConvert.this.desDatasource == null) {
+					return false;
+				}
+
+				return JDialogGeometryConvert.this.desDatasource.getDatasets().isAvailableDatasetName(JDialogGeometryConvert.this.newDatasetName);
+			}
+
+			@Override
+			public String getLegitValue(String currentValue, String backUpValue) {
+				return backUpValue;
+			}
+		});
 
 		GroupLayout gl = new GroupLayout(getContentPane());
 		gl.setAutoCreateContainerGaps(true);
@@ -107,16 +232,101 @@ public class JDialogGeometryConvert extends SmDialog {
 		// @formatter:on
 	}
 
-	public static void main(String[] args) {
-		final JDialogGeometryConvert dialog = new JDialogGeometryConvert("test");
-		dialog.setSize(400, 200);
+	private void registerEvents() {
+		this.comboBoxDatasource.addItemListener(this.itemListener);
+		this.comboBoxDataset.addItemListener(this.itemListener);
+		this.checkBoxNewDataset.addItemListener(itemListener);
+		this.checkBoxRemoveSrc.addItemListener(this.itemListener);
+		this.textFieldNewDataset.getDocument().addDocumentListener(this.documentListener);
+		this.buttonOK.addActionListener(this.actionListener);
+		this.buttonCancel.addActionListener(this.actionListener);
+	}
 
-		SwingUtilities.invokeLater(new Runnable() {
+	private void initializeComponentsValue() {
+		this.comboBoxDatasource.setSelectedDatasource(this.desDatasource);
+		initializeComboBoxDatasetValue();
+		this.checkBoxNewDataset.setSelected(this.isNewDataset);
+		this.checkBoxRemoveSrc.setSelected(this.isRemoveSrc);
+		this.textFieldNewDataset.setText(this.newDatasetName);
+	}
 
-			@Override
-			public void run() {
-				dialog.setVisible(true);
+	private void initializeComboBoxDatasetValue() {
+		this.comboBoxDataset.removeAllItems();
+
+		if (this.desDatasource != null) {
+			for (int i = 0; i < this.desDatasource.getDatasets().getCount(); i++) {
+				Dataset dataset = this.desDatasource.getDatasets().get(i);
+
+				if (dataset.getType() == this.datasetType) {
+					DataCell cell = new DataCell();
+					cell.initDatasetType(dataset);
+					this.comboBoxDataset.addItem(cell);
+				}
 			}
-		});
+		}
+		this.comboBoxDataset.setSelectedDataset(this.desDataset);
+	}
+
+	private void comboBoxDesDatasourceSelectedChange() {
+		try {
+			this.desDatasource = this.comboBoxDatasource.getSelectedDatasource();
+
+			// 重新初始化数据集组合框
+			initializeDesDataset();
+			this.comboBoxDataset.removeItemListener(this.itemListener);
+			initializeComboBoxDatasetValue();
+
+			// 重新初始化文本框
+			this.textFieldNewDataset.getDocument().removeDocumentListener(this.documentListener);
+			this.textFieldNewDataset.setText(this.desDatasource.getDatasets().getAvailableDatasetName(this.newDatasetName));
+
+			setComponentsEnabled();
+		} catch (Exception e) {
+			Application.getActiveApplication().getOutput().output(e);
+		} finally {
+			this.comboBoxDataset.addItemListener(this.itemListener);
+			this.textFieldNewDataset.getDocument().addDocumentListener(this.documentListener);
+		}
+	}
+
+	private void comboBoxDesDatasetSelectedChange() {
+		this.desDataset = (DatasetVector) this.comboBoxDataset.getSelectedDataset();
+	}
+
+	private void textFieldNewDatasetTextChange() {
+		if (this.textFieldNewDataset.isLegitValue(this.textFieldNewDataset.getText())) {
+			this.newDatasetName = this.textFieldNewDataset.getText();
+		}
+		setComponentsEnabled();
+	}
+
+	private void checkBoxNewDatasetCheckedChange() {
+		this.isNewDataset = this.checkBoxNewDataset.isSelected();
+		setComponentsEnabled();
+	}
+
+	private void checkBoxRemoveSrcCheckedChange() {
+		this.isRemoveSrc = this.checkBoxRemoveSrc.isSelected();
+	}
+
+	private void setComponentsEnabled() {
+		this.textFieldNewDataset.setEnabled(this.desDatasource != null && this.isNewDataset);
+		this.comboBoxDataset.setEnabled(this.desDatasource != null && !this.isNewDataset);
+
+		if (this.isNewDataset) {
+			this.buttonOK.setEnabled(this.desDatasource != null && this.textFieldNewDataset.isLegitValue(this.newDatasetName));
+		} else {
+			this.buttonOK.setEnabled(this.desDataset != null);
+		}
+	}
+
+	private void buttonOKClick() {
+		setDialogResult(DialogResult.OK);
+		setVisible(false);
+	}
+
+	private void buttonCancelClick() {
+		setDialogResult(DialogResult.CANCEL);
+		setVisible(false);
 	}
 }
