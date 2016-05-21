d8d8a61711b70fe1366f5c9ccd1fbe682b201624
highsad <wuxiaobo@supermap.com>
2016-05-21 18:03:55 +0800

    线转面基本功能代码。

--- MapEditor/src/com/supermap/desktop/geometryoperation/CtrlAction/CtrlActionLineToRegion.java
+++ MapEditor/src/com/supermap/desktop/geometryoperation/CtrlAction/CtrlActionLineToRegion.java
@@ -2,12 +2,20 @@ package com.supermap.desktop.geometryoperation.CtrlAction;
 
 import com.supermap.desktop.Interface.IBaseItem;
 import com.supermap.desktop.Interface.IForm;
+import com.supermap.desktop.geometryoperation.editor.IEditor;
+import com.supermap.desktop.geometryoperation.editor.LineToRegionEditor;
 
 public class CtrlActionLineToRegion extends CtrlActionEditorBase {
+
+	private LineToRegionEditor editor = new LineToRegionEditor();
 
 	public CtrlActionLineToRegion(IBaseItem caller, IForm formClass) {
 		super(caller, formClass);
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public IEditor getEditor() {
+		return this.editor;
+	}
 }
--- MapEditor/src/com/supermap/desktop/geometryoperation/editor/LineToRegionEditor.java
+++ MapEditor/src/com/supermap/desktop/geometryoperation/editor/LineToRegionEditor.java
@@ -1,5 +1,44 @@
 package com.supermap.desktop.geometryoperation.editor;
 
-public class LineToRegionEditor extends AbstractEditor {
+import java.util.Map;
 
+import com.supermap.data.DatasetType;
+import com.supermap.data.Recordset;
+import com.supermap.desktop.geometry.Abstract.IGeometry;
+import com.supermap.desktop.geometry.Abstract.ILineFeature;
+import com.supermap.desktop.geometryoperation.EditEnvironment;
+import com.supermap.desktop.mapeditor.MapEditorProperties;
+import com.supermap.desktop.utilties.ListUtilties;
+
+public class LineToRegionEditor extends GeometryConvertEditor {
+
+	@Override
+	public String getTitle() {
+		return MapEditorProperties.getString("String_GeometryOperation_LineToRegion");
+	}
+
+	@Override
+	public DatasetType getDesDatasetType() {
+		return DatasetType.REGION;
+	}
+
+	@Override
+	public DatasetType getSrcDatasetType() {
+		return DatasetType.LINE;
+	}
+
+	@Override
+	public boolean convert(Recordset desRecordset, IGeometry srcGeometry, Map<String, Object> properties) {
+		boolean isConverted = true;
+
+		if (srcGeometry instanceof ILineFeature) {
+
+		}
+		return isConverted;
+	}
+
+	@Override
+	public boolean enble(EditEnvironment environment) {
+		return ListUtilties.isListContainAny(environment.getEditProperties().getSelectedGeometryTypeFeatures(), ILineFeature.class);
+	}
 }
