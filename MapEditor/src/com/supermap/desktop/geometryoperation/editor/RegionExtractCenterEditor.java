9b061589b0b6f9138330023462bb66abb4efeca4
highsad <532209081@qq.com>
2016-05-22 21:28:18 +0800

    新增对象编辑-面提取边界线、对象编辑-面提取中心线。

---
+++ MapEditor/src/com/supermap/desktop/geometryoperation/editor/RegionExtractCenterEditor.java
@@ -0,0 +1,5 @@
+package com.supermap.desktop.geometryoperation.editor;
+
+public class RegionExtractCenterEditor extends AbstractEditor {
+
+}
--- MapEditor/src/com/supermap/desktop/geometryoperation/editor/RegionToLineEditor.java
+++
@@ -1,5 +0,0 @@
-package com.supermap.desktop.geometryoperation.editor;
-
-public class RegionToLineEditor extends AbstractEditor {
-
-}
