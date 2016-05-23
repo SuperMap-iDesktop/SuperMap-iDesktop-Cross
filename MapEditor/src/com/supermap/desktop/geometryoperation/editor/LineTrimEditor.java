7e2213108b1368d5c21b1ebb4feeac17abdc9b36
highsad <wuxiaobo@supermap.com>
2016-05-23 11:44:12 +0800

    新增对象编辑-修剪配置项。

---
+++ MapEditor/src/com/supermap/desktop/geometryoperation/editor/LineTrimEditor.java
@@ -0,0 +1,21 @@
+package com.supermap.desktop.geometryoperation.editor;
+
+import com.supermap.desktop.geometryoperation.EditEnvironment;
+
+public class LineTrimEditor extends AbstractEditor {
+
+	@Override
+	public void activate(EditEnvironment environment) {
+		// 按需重写
+	}
+
+	@Override
+	public void deactivate(EditEnvironment environment) {
+		// 按需重写
+	}
+
+	@Override
+	public boolean enble(EditEnvironment environment) {
+		return false;
+	}
+}
