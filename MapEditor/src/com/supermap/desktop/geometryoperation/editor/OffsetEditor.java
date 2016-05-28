f418aae881b946b8a416c70ec8a06b2168e6b18e
highsad <wuxiaobo@supermap.com>
2016-05-28 18:13:13 +0800

    对象编辑-缩放功能实现。

--- MapEditor/src/com/supermap/desktop/geometryoperation/editor/OffsetEditor.java
+++ MapEditor/src/com/supermap/desktop/geometryoperation/editor/OffsetEditor.java
@@ -1,5 +1,28 @@
 package com.supermap.desktop.geometryoperation.editor;
 
+import com.supermap.data.DatasetType;
+import com.supermap.desktop.geometryoperation.EditEnvironment;
+import com.supermap.desktop.utilties.ListUtilties;
+
 public class OffsetEditor extends AbstractEditor {
 
+	@Override
+	public void activate(EditEnvironment environment) {
+		// 按需重写
+	}
+
+	@Override
+	public void deactivate(EditEnvironment environment) {
+		// 按需重写
+	}
+
+	@Override
+	public boolean enble(EditEnvironment environment) {
+		return ListUtilties.isListContainAny(environment.getEditProperties().getEditableDatasetTypes(), DatasetType.LINE, DatasetType.REGION, DatasetType.CAD);
+	}
+
+	@Override
+	public boolean check(EditEnvironment environment) {
+		return environment.getEditor() instanceof OffsetEditor;
+	}
 }
