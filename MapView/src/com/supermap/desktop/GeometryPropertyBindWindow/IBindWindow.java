aff08d3953446a551442befa563bb1eb4cfffe9a
xiexiuji <xiexiuji@supermap.com>
2016-06-14 11:13:57 +0800

    关联浏览缺陷处理

---
+++ MapView/src/com/supermap/desktop/GeometryPropertyBindWindow/IBindWindow.java
@@ -0,0 +1,46 @@
+package com.supermap.desktop.GeometryPropertyBindWindow;
+
+import com.supermap.data.Dataset;
+import com.supermap.mapping.Selection;
+
+/**
+ * 关联窗口接口
+ * 
+ * @author xie
+ *
+ */
+public interface IBindWindow {
+
+	/**
+	 * 选择集是否变化
+	 * 
+	 * @return
+	 */
+	public boolean isSelectionHasChanged();
+
+	/**
+	 * 设置当前活动的数据集
+	 * 
+	 * @param dataset
+	 */
+	public void setActiveDataset(Dataset dataset);
+
+	/**
+	 * 刷新属性表
+	 */
+	public void refreshFormTabular(int[] addRows);
+	/**
+	 * 移除事件
+	 */
+	public void removeEvents();
+	/**
+	 * 销毁类，释放资源
+	 */
+	public void dispose();
+	
+	public void addMapSelectionChangeListener(MapSelectionChangeListener l);
+	
+	public void removeMapSelectionChangeListener(MapSelectionChangeListener l);
+	
+	public void fireSelectionChanged(Selection selection);
+}
