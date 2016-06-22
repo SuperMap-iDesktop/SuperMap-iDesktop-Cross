a4e91c8af207504093dff3c7955b39db6d214378
highsad <wuxiaobo@supermap.com>
2016-06-22 14:05:52 +0800

    面提取中心线执行完添加到当前地图。

--- MapEditor/src/com/supermap/desktop/geometryoperation/editor/RegionExtractCenterEditor.java
+++ MapEditor/src/com/supermap/desktop/geometryoperation/editor/RegionExtractCenterEditor.java
@@ -1,13 +1,16 @@
 package com.supermap.desktop.geometryoperation.editor;
 
+import java.awt.Color;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 
 import com.supermap.analyst.spatialanalyst.Generalization;
+import com.supermap.data.Dataset;
 import com.supermap.data.DatasetType;
 import com.supermap.data.DatasetVector;
 import com.supermap.data.Datasource;
 import com.supermap.data.EditType;
+import com.supermap.data.GeoStyle;
 import com.supermap.data.Recordset;
 import com.supermap.desktop.Application;
 import com.supermap.desktop.geometryoperation.EditEnvironment;
@@ -18,6 +21,7 @@ import com.supermap.desktop.utilities.CursorUtilities;
 import com.supermap.desktop.utilities.ListUtilities;
 import com.supermap.desktop.utilities.MapUtilities;
 import com.supermap.mapping.Layer;
+import com.supermap.mapping.LayerSettingVector;
 
 /**
  * 面提取中心线仅选中单图层对象时可用，并且不支持 CAD
@@ -34,10 +38,11 @@ public class RegionExtractCenterEditor extends AbstractEditor {
 
 			if (dialog.showDialog() == DialogResult.OK) {
 				CursorUtilities.setWaitCursor(environment.getMapControl());
-				boolean isConverted = convert(environment, dialog.getDesDatasource(), dialog.getNewDatasetName(), dialog.getMax(), dialog.getMin(),
+				DatasetVector datasetVector = convert(environment, dialog.getDesDatasource(), dialog.getNewDatasetName(), dialog.getMax(), dialog.getMin(),
 						dialog.isRemoveSrc());
 
-				if (isConverted) {
+				if (datasetVector != null) {
+					addToCurrentMap(environment, datasetVector);
 					Application
 							.getActiveApplication()
 							.getOutput()
@@ -65,8 +70,9 @@ public class RegionExtractCenterEditor extends AbstractEditor {
 				&& ListUtilities.isListOnlyContain(environment.getEditProperties().getSelectedDatasetTypes(), DatasetType.REGION);
 	}
 
-	private boolean convert(EditEnvironment environment, Datasource desDatasource, String newDatasetName, double maxWidth, double minWidth, boolean isRemoveSrc) {
-		boolean isConverted = false;
+	private DatasetVector convert(EditEnvironment environment, Datasource desDatasource, String newDatasetName, double maxWidth, double minWidth,
+			boolean isRemoveSrc) {
+		DatasetVector datasetVector = null;
 
 		try {
 			ArrayList<Layer> layers = MapUtilities.getLayers(environment.getMap());
@@ -83,10 +89,9 @@ public class RegionExtractCenterEditor extends AbstractEditor {
 
 					try {
 						// 提取中心线
-						DatasetVector datasetVector = Generalization.dualLineToCenterLine(srcRecordset, maxWidth, minWidth, desDatasource, newDatasetName);
+						datasetVector = Generalization.dualLineToCenterLine(srcRecordset, maxWidth, minWidth, desDatasource, newDatasetName);
 
 						if (datasetVector != null) {
-							isConverted = true;
 							// 图层可编辑才能移除源对象
 							if (isRemoveSrc && layer.isEditable()) {
 								environment.getMapControl().getEditHistory().batchBegin();
@@ -111,11 +116,24 @@ public class RegionExtractCenterEditor extends AbstractEditor {
 				}
 			}
 		} catch (Exception e) {
-			isConverted = false;
 			Application.getActiveApplication().getOutput().output(e);
 		} finally {
 			environment.getMap().refresh();
 		}
-		return isConverted;
+		return datasetVector;
+	}
+
+	private void addToCurrentMap(EditEnvironment environment, Dataset dataset) {
+
+		// 设置线图层风格
+		GeoStyle geoStyle = new GeoStyle();
+		geoStyle.setLineColor(new Color(66, 183, 213));
+		geoStyle.setLineWidth(1);
+		LayerSettingVector layerSettingVector = new LayerSettingVector();
+		layerSettingVector.setStyle(geoStyle);
+
+		// 添加到当前地图
+		Layer layer = environment.getMap().getLayers().add(dataset, layerSettingVector, true);
+		environment.getMap().refresh();
 	}
 }
