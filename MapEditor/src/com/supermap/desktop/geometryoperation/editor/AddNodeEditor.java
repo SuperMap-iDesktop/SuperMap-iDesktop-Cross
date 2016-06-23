82b41da18a2fc2b0e26134e5d2fe87b78e24a026
highsad <wuxiaobo@supermap.com>
2016-06-23 15:54:06 +0800

    解决添加节点在某些情况下会点击2次才能激活功能的问题。

--- MapEditor/src/com/supermap/desktop/geometryoperation/editor/AddNodeEditor.java
+++ MapEditor/src/com/supermap/desktop/geometryoperation/editor/AddNodeEditor.java
@@ -1,20 +1,42 @@
 package com.supermap.desktop.geometryoperation.editor;
 
 import com.supermap.data.GeometryType;
+import com.supermap.desktop.geometryoperation.EditControllerAdapter;
 import com.supermap.desktop.geometryoperation.EditEnvironment;
+import com.supermap.desktop.geometryoperation.IEditController;
+import com.supermap.desktop.geometryoperation.NullEditController;
 import com.supermap.desktop.utilities.ListUtilities;
 import com.supermap.ui.Action;
+import com.supermap.ui.ActionChangedEvent;
 
 public class AddNodeEditor extends AbstractEditor {
+
+	private IEditController addNodeEditController = new EditControllerAdapter() {
+
+		@Override
+		public void actionChanged(EditEnvironment environment, ActionChangedEvent e) {
+			if (e.getOldAction() == Action.VERTEXADD) {
+
+				// @formatter:off
+				// 组件在很多情况下会自动结束编辑状态，比如右键，比如框选一堆对象，
+				// 比如当前操作对象所在图层变为不可编辑状态，这时候桌面自定义的 Editor 还没有结束编辑，处理一下
+				// @formatter:on
+				environment.stopEditor();
+			}
+		}
+	};
 
 	@Override
 	public void activate(EditEnvironment environment) {
 		environment.getMapControl().setAction(Action.VERTEXADD);
+		environment.setEditController(this.addNodeEditController);
+		environment.getMap().refresh();
 	}
 
 	@Override
 	public void deactivate(EditEnvironment environment) {
 		environment.getMapControl().setAction(Action.SELECT2);
+		environment.setEditController(NullEditController.instance());
 	}
 
 	@Override
