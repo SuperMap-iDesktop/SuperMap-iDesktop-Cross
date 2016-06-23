fc64e9a14a058131b92fbd971de11b2742699dd9
highsad <wuxiaobo@supermap.com>
2016-06-23 15:16:28 +0800

    1. 解决节点编辑右键结束之后要点击2次才能再次激活的问题。 2. 解决节点编辑开启之后，选中对象不是编辑状态的问题。

--- MapEditor/src/com/supermap/desktop/geometryoperation/editor/ModifyNodeEditor.java
+++ MapEditor/src/com/supermap/desktop/geometryoperation/editor/ModifyNodeEditor.java
@@ -1,20 +1,41 @@
 package com.supermap.desktop.geometryoperation.editor;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+import javax.swing.SwingUtilities;
+
 import com.supermap.data.GeometryType;
+import com.supermap.desktop.geometryoperation.EditControllerAdapter;
 import com.supermap.desktop.geometryoperation.EditEnvironment;
+import com.supermap.desktop.geometryoperation.IEditController;
+import com.supermap.desktop.geometryoperation.NullEditController;
 import com.supermap.desktop.utilities.ListUtilities;
 import com.supermap.ui.Action;
 
 public class ModifyNodeEditor extends AbstractEditor {
 
+	private IEditController modifyNodeEditController = new EditControllerAdapter() {
+
+		@Override
+		public void mouseClicked(EditEnvironment environment, MouseEvent e) {
+			if (SwingUtilities.isRightMouseButton(e)) {
+				environment.stopEditor();
+			}
+		}
+	};
+
 	@Override
 	public void activate(EditEnvironment environment) {
 		environment.getMapControl().setAction(Action.VERTEXEDIT);
+		environment.setEditController(this.modifyNodeEditController);
+		environment.getMap().refresh();
 	}
 
 	@Override
 	public void deactivate(EditEnvironment environment) {
 		environment.getMapControl().setAction(Action.SELECT2);
+		environment.setEditController(NullEditController.instance());
 	}
 
 	@Override
