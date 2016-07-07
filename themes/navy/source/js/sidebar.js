507a0a3d9519fdcda94cd9ed24e741d47361cbf7
qinlj <qinlijuan@supermap.com>
2016-07-07 21:13:15 +0800

    目录定位高亮显示，优化字体显示效果

--- themes/navy/source/js/sidebar.js
+++ themes/navy/source/js/sidebar.js
@@ -20,7 +20,6 @@
 			closedSign: '&nbsp;&nbsp;<i class="fa fa-chevron-circle-right"></i>',
 			openedSign: '&nbsp;&nbsp;<i class="fa fa-chevron-circle-down"></i>'
 		};
-
 		// Extend our default options with those provided.
 		var opts = $.extend(defaults, options);
 		//Assign current element to variable, in this case is UL element
@@ -84,33 +83,53 @@
   				}
   			}
   		});
-		$this.find("li a").click(function () {
-			function OpenNode() {
-				var OpenNodeId = '';
-				$this.find("li strong").each(function() {
-					if ($(this).parent().find("ul:first").is(":visible") != 0) {
-						if(OpenNodeId==''){
-							OpenNodeId= $(this).parent().attr('id');
-						}else {
-							OpenNodeId +=',' + $(this).parent().attr('id');
-						}
-					}
-				});
-				return OpenNodeId;
-			}
-			var redhref = $(this).attr('href');
-			redhref += '?'+OpenNode();
-			$(this).attr('href',redhref);
-		});
 
-		var UrlParam = window.location.search.substr(1);
-		var UrlParamArray = UrlParam.split(',');
-		for (var i= 0;i < UrlParamArray.length;i++) {
-			var nodeid = document.getElementById(UrlParamArray[i]);
-			var nodeidjQuery = $(nodeid);
-			nodeidjQuery.find("ul:first").show();
-			nodeidjQuery.parent().find("li:first").find("span:first").html(opts.openedSign);
+		function sidebarUnfold() {
+			$this.find("li a").click(function () {
+				function OpenNode() {
+					var OpenNodeId = '';
+					$this.find("li strong").each(function() {
+						if ($(this).parent().find("ul:first").is(":visible") != 0) {
+							if(OpenNodeId==''){
+								OpenNodeId= $(this).parent().attr('id');
+							}else {
+								OpenNodeId +=',' + $(this).parent().attr('id');
+							}
+						}
+					});
+					return OpenNodeId;
+				}
+
+				var redhref = $(this).attr('href');
+				redhref += '?'+OpenNode();
+				$(this).attr('href',redhref);
+			});
+
+			var UrlParam = window.location.search.substr(1);
+			var UrlParamArray = UrlParam.split(',');
+			for (var i= 0;i < UrlParamArray.length;i++) {
+				var nodeid = document.getElementById(UrlParamArray[i]);
+				var nodeidjQuery = $(nodeid);
+				nodeidjQuery.find("ul:first").show();
+				nodeidjQuery.find("span:first").html(opts.openedSign);
 			}
+		}
+		sidebarUnfold();
+
+		function sidebarHighlight() {
+			var currenthref = window.location.href;
+			var hrefparams = currenthref.split("/");
+			var hrefparam1 = hrefparams[hrefparams.length-1];
+			var hrefparam2 = hrefparam1.split(".");
+			var filename = hrefparam2[hrefparam2.length-2];
+			$this.find("li").each(function () {
+				if ($(this).attr('id') == filename) {
+					$(this).find("a").css('color','#33a3dc');
+				}
+			});
+		}
+		sidebarHighlight();
+
 		}
 
 	});
