537bbea5021f86986a7a072f3cf60b0236749700
xiajt <xiajintao@supermap.com>
2016-07-08 10:20:39 +0800

    新建配准界面

--- Assistant/src/com/supermap/desktop/FormTransformation.java
+++ Assistant/src/com/supermap/desktop/FormTransformation.java
@@ -3,16 +3,32 @@ package com.supermap.desktop;
 import com.supermap.data.Dataset;
 import com.supermap.data.Datasource;
 import com.supermap.desktop.Interface.IFormTransformation;
+import com.supermap.desktop.controls.utilities.MapViewUIUtilities;
 import com.supermap.desktop.enums.WindowType;
 import com.supermap.desktop.ui.FormBaseChild;
+import com.supermap.desktop.ui.controls.GridBagConstraintsHelper;
+import com.supermap.ui.MapControl;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.util.ArrayList;
 
 /**
  * @author XiaJT
  */
 public class FormTransformation extends FormBaseChild implements IFormTransformation {
+	private MapControl mapControlTransformation;
+	private MapControl mapControlReference;
+	private Datasource resultDatasource;
+	private String resultDatasetName;
+	private Dataset transformationDataset;
+	private java.util.List<Dataset> referenceDatasetList;
+	private JTable tablePoints;
+	private JSplitPane splitPaneMapControls;
+	private JSplitPane splitPaneMain;
+
 	public FormTransformation() {
 		this(null);
 
@@ -25,7 +41,48 @@ public class FormTransformation extends FormBaseChild implements IFormTransforma
 
 	public FormTransformation(String name, Icon icon, Component component) {
 		super(name, icon, component);
+		mapControlTransformation = new MapControl();
+		mapControlTransformation.getMap().setWorkspace(Application.getActiveApplication().getWorkspace());
+		mapControlReference = new MapControl();
+		mapControlReference.getMap().setWorkspace(Application.getActiveApplication().getWorkspace());
+		referenceDatasetList = new ArrayList<>();
+		tablePoints = new JTable();
+		initLayout();
+		initListener();
+	}
 
+	private void initLayout() {
+		this.setLayout(new GridBagLayout());
+		this.splitPaneMapControls = new JSplitPane();
+		this.splitPaneMapControls.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
+		this.splitPaneMapControls.setLeftComponent(mapControlTransformation);
+		this.splitPaneMapControls.setRightComponent(mapControlReference);
+
+		this.splitPaneMain = new JSplitPane();
+		this.splitPaneMain.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		this.splitPaneMain.setLeftComponent(this.splitPaneMapControls);
+		this.splitPaneMain.setRightComponent(new JScrollPane(tablePoints));
+
+		this.add(this.splitPaneMain, new GridBagConstraintsHelper(0, 0, 1, 1).setFill(GridBagConstraints.BOTH).setWeight(1, 1));
+		// FIXME: 2016/7/8 状态栏为空
+//		this.add(getStatusbar(), new GridBagConstraintsHelper(0, 1, 1, 1).setFill(GridBagConstraints.HORIZONTAL).setWeight(1, 0));
+	}
+
+	private void initListener() {
+		splitPaneMain.addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent e) {
+				splitPaneMain.setDividerLocation(0.8);
+				splitPaneMain.removeComponentListener(this);
+			}
+		});
+		splitPaneMapControls.addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent e) {
+				splitPaneMapControls.setDividerLocation(0.5);
+				splitPaneMapControls.removeComponentListener(this);
+			}
+		});
 	}
 
 	@Override
@@ -45,12 +102,12 @@ public class FormTransformation extends FormBaseChild implements IFormTransforma
 
 	@Override
 	public boolean save() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean save(boolean notify, boolean isNewWindow) {
-		return false;
+		return true;
 	}
 
 	@Override
@@ -91,6 +148,7 @@ public class FormTransformation extends FormBaseChild implements IFormTransforma
 	@Override
 	public void windowShown() {
 
+
 	}
 
 	@Override
@@ -105,21 +163,25 @@ public class FormTransformation extends FormBaseChild implements IFormTransforma
 
 	@Override
 	public void setTransformationDataset(Dataset transformationDataset) {
-
+		this.transformationDataset = transformationDataset;
+		MapViewUIUtilities.addDatasetsToMap(mapControlTransformation.getMap(), new Dataset[]{transformationDataset}, true);
 	}
 
 	@Override
 	public void addReferenceDataset(Dataset referenceDataset) {
-
+		if (referenceDataset != null) {
+			referenceDatasetList.add(referenceDataset);
+			MapViewUIUtilities.addDatasetsToMap(mapControlReference.getMap(), new Dataset[]{referenceDataset}, true);
+		}
 	}
 
 	@Override
 	public void setResultDataSource(Datasource resultDatasource) {
-
+		this.resultDatasource = resultDatasource;
 	}
 
 	@Override
 	public void setResultDatasetName(String resultDatasetName) {
-
+		this.resultDatasetName = resultDatasetName;
 	}
 }
