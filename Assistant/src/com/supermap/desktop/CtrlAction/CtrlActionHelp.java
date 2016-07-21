b00d29731bc359d1a082cf23c4540fcabf13ee2a
highsad <wuxiaobo@supermap.com>
2016-07-21 10:30:07 +0800

    1. 更换联机帮助链接； 2. 暂时移除缩放功能。

--- Assistant/src/com/supermap/desktop/CtrlAction/CtrlActionHelp.java
+++ Assistant/src/com/supermap/desktop/CtrlAction/CtrlActionHelp.java
@@ -1,15 +1,9 @@
 package com.supermap.desktop.CtrlAction;
 
-import java.io.IOException;
-
 import com.supermap.desktop.Application;
 import com.supermap.desktop.Interface.IBaseItem;
-import com.supermap.desktop.Interface.ICtrlAction;
-import com.supermap.desktop.Interface.IDockbar;
 import com.supermap.desktop.Interface.IForm;
-import com.supermap.desktop.Interface.IFormMain;
 import com.supermap.desktop.implement.CtrlAction;
-import com.supermap.desktop.ui.controls.DockbarManager;
 import com.supermap.desktop.utilities.BrowseUtilities;
 
 public class CtrlActionHelp extends CtrlAction {
@@ -21,7 +15,7 @@ public class CtrlActionHelp extends CtrlAction {
 	@Override
 	public void run() {
 		try {
-			BrowseUtilities.openUrl("http://support.supermap.com.cn/product/iDesktop.aspx");
+			BrowseUtilities.openUrl("http://support.supermap.com.cn/SuperMap-iDesktop-Cross/");
 		} catch (Exception e) {
 			Application.getActiveApplication().getOutput().output(e);
 		}
