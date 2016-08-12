21700b9b5b83e0d0a29c2d3a25849af859070f9b
xiexiuji <xiexiuji@supermap.com>
2016-08-12 09:00:45 +0800

    CAD复合风格

--- MapEditor/src/main/java/com/supermap/desktop/CtrlAction/CADStyle/CADStyleEditor.java
+++ MapEditor/src/main/java/com/supermap/desktop/CtrlAction/CADStyle/CADStyleEditor.java
@@ -1,12 +1,13 @@
 package com.supermap.desktop.CtrlAction.CADStyle;
 
 import com.supermap.desktop.geometryoperation.EditEnvironment;
+import com.supermap.desktop.geometryoperation.editor.AbstractEditor;
 import com.supermap.desktop.geometryoperation.editor.IEditor;
 
 /**
  * Created by xie on 2016/8/10.
  */
-public class CADStyleEditor implements IEditor{
+public class CADStyleEditor extends AbstractEditor {
 
     @Override
     public void activate(EditEnvironment environment) {
@@ -21,11 +22,11 @@ public class CADStyleEditor implements IEditor{
 
     @Override
     public boolean enble(EditEnvironment environment) {
-        return false;
+        return true;
     }
 
     @Override
     public boolean check(EditEnvironment environment) {
-        return false;
+        return true;
     }
 }
