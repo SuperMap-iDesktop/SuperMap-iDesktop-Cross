3ad6bda06bd6808bfbb91004dc7ec59369884e1a
highsad <532209081@qq.com>
2016-08-25 00:44:55 +0800

    移除无用依赖。

--- DataView/src/main/java/com/supermap/desktop/framemenus/CtrlActionDatasourceMonopolyMode.java
+++ DataView/src/main/java/com/supermap/desktop/framemenus/CtrlActionDatasourceMonopolyMode.java
@@ -1,20 +1,7 @@
 package com.supermap.desktop.framemenus;
 
-import com.supermap.data.Datasource;
-import com.supermap.data.DatasourceConnectionInfo;
-import com.supermap.data.EngineType;
-import com.supermap.data.Workspace;
-import com.supermap.desktop.Application;
 import com.supermap.desktop.Interface.IBaseItem;
 import com.supermap.desktop.Interface.IForm;
-import com.supermap.desktop.controls.utilities.DatasourceOpenFileUtilties;
-import com.supermap.desktop.implement.CtrlAction;
-import com.supermap.desktop.ui.UICommonToolkit;
-import com.supermap.desktop.ui.controls.WorkspaceTree;
-import com.supermap.desktop.utilities.DatasourceUtilities;
-
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreePath;
 
 /**
  * Created by highsad on 2016/8/25.
