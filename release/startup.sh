1997999c146ec8e8dd6c0e978d7f3dd93ebaafa8
highsad <wuxiaobo@supermap.com>
2016-08-30 10:23:32 +0800

    移除错误的 Linux 启动文件。

--- release/startup.sh
+++
@@ -1,5 +0,0 @@
-@echo off
-export LD_LIBRARY_PATH=./bin:$LD_LIBRARY_PATH
-export JRE_PATH=./jre/bin
-export PATH=$LD_LIBRARY_PATH:$JRE_PATH:$PATH
-java -Dfelix.config.properties=file:Configuration/config.properties -splash:./Resources/Frame/iDesktop_Cross_Startup.gif -jar iDesktop.jar
