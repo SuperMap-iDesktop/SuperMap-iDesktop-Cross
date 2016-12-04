37b8c928285d0d75dc9aa1b32265d7e927270740
highsad <532209081@qq.com>
2016-12-05 00:55:15 +0800

    调整一下子窗口属性。

--- Controls/src/main/java/com/supermap/desktop/ui/mdi/plaf/properties/MdiGroupUIProperties.java
+++ Controls/src/main/java/com/supermap/desktop/ui/mdi/plaf/properties/MdiGroupUIProperties.java
@@ -6,5 +6,5 @@ import java.awt.*;
  * Created by highsad on 2016/9/20.
  */
 public class MdiGroupUIProperties {
-	public static Insets INSETS = new Insets(10, 10, 10, 10);
+	public static Insets INSETS = new Insets(0, 0, 0, 0);
 }
--- Controls/src/main/java/com/supermap/desktop/ui/mdi/plaf/properties/MdiTabsUIProperties.java
+++ Controls/src/main/java/com/supermap/desktop/ui/mdi/plaf/properties/MdiTabsUIProperties.java
@@ -6,6 +6,6 @@ import java.awt.*;
  * Created by highsad on 2016/9/20.
  */
 public class MdiTabsUIProperties {
-	public static Insets INSETS = new Insets(0, 8, 0, 4);
+	public static Insets INSETS = new Insets(0, 4, 0, 4);
 	public static int TAB_GAP = 2; // tab 与 tab 之间的距离
 }
