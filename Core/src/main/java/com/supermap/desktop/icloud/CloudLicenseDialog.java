dcaa4e42093bdeb709cd0b8ded69e88ae77f50c0
xiajt <xiajintao@supermap.com>
2016-12-28 14:42:32 +0800

    云许可修改。

--- Core/src/main/java/com/supermap/desktop/icloud/CloudLicenseDialog.java
+++ Core/src/main/java/com/supermap/desktop/icloud/CloudLicenseDialog.java
@@ -6,20 +6,25 @@ import com.supermap.desktop.icloud.api.LicenseService;
 import com.supermap.desktop.icloud.commontypes.ApplyFormalLicenseResponse;
 import com.supermap.desktop.icloud.commontypes.ApplyTrialLicenseResponse;
 import com.supermap.desktop.icloud.commontypes.LicenseId;
-import com.supermap.desktop.icloud.commontypes.ProductType;
-import com.supermap.desktop.icloud.impl.LicenseServiceFactory;
-import com.supermap.desktop.icloud.online.AuthenticationException;
-import com.supermap.desktop.properties.CommonProperties;
 import com.supermap.desktop.properties.CoreProperties;
 import com.supermap.desktop.ui.controls.GridBagConstraintsHelper;
 import com.supermap.desktop.utilities.CoreResources;
-import com.supermap.desktop.utilities.JOptionPaneUtilities;
 import com.supermap.desktop.utilities.PathUtilities;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URI;
 
 /**
@@ -97,30 +102,30 @@ public class CloudLicenseDialog extends JDialog {
     private void login() {
         userName = textFieldUserName.getText();
         passWord = String.valueOf(fieldPassWord.getPassword());
-        try {
-//        CloudLicense.login(userName, passWord);
-//        saveToken();
-        dialogResult = DIALOGRESULT_OK;
-            licenseService = LicenseServiceFactory.create(userName, passWord, ProductType.IDESKTOP);
-            licenseId = LicenseManager.getFormalLicenseId(licenseService);
-            if (null != licenseId) {
-                //有正式许可id，则申请正式许可
-                formalLicenseResponse = LicenseManager.applyFormalLicense(licenseService, licenseId);
-                dialogResult = DIALOGRESULT_OK;
-                saveToken();
-            } else {
-                //没有正式许可id,则申请试用许可
-                trialLicenseResponse = LicenseManager.applyTrialLicense(licenseService);
-                dialogResult = DIALOGRESULT_OK;
-                saveToken();
-            }
-        } catch (AuthenticationException e1) {
-            JOptionPaneUtilities.showMessageDialog(CommonProperties.getString("String_PermissionCheckFailed"));
-            dialogResult = DIALOGRESULT_CANCEL;
-        } finally {
-            removeEvents();
-            dispose();
-        }
+//        try {
+	    CloudLicense.login(userName, passWord);
+	    saveToken();
+	    dialogResult = DIALOGRESULT_OK;
+//            licenseService = LicenseServiceFactory.create(userName, passWord, ProductType.IDESKTOP);
+//            licenseId = LicenseManager.getFormalLicenseId(licenseService);
+//            if (null != licenseId) {
+//                //有正式许可id，则申请正式许可
+//                formalLicenseResponse = LicenseManager.applyFormalLicense(licenseService, licenseId);
+//                dialogResult = DIALOGRESULT_OK;
+//                saveToken();
+//            } else {
+//                //没有正式许可id,则申请试用许可
+//                trialLicenseResponse = LicenseManager.applyTrialLicense(licenseService);
+//                dialogResult = DIALOGRESULT_OK;
+//                saveToken();
+//            }
+//        } catch (AuthenticationException e1) {
+//            JOptionPaneUtilities.showMessageDialog(CommonProperties.getString("String_PermissionCheckFailed"));
+//            dialogResult = DIALOGRESULT_CANCEL;
+//        } finally {
+//            removeEvents();
+//            dispose();
+//        }
     }
 
     private void saveToken() {
