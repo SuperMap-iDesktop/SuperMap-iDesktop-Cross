69dba4c51456cfef790f3ff8d4e06feef9d86b2f
lixiaoyao <liwenfa@supermap.com>
2017-02-13 13:43:43 +0800

    数据集字段选择table封装优化——李文发

--- Controls/src/main/java/com/supermap/desktop/ui/controls/ChooseTable/SmChooseTable.java
+++ Controls/src/main/java/com/supermap/desktop/ui/controls/ChooseTable/SmChooseTable.java
@@ -1,7 +1,7 @@
 package com.supermap.desktop.ui.controls.ChooseTable;
 
-import com.supermap.desktop.Application;
-import com.supermap.desktop.controls.ControlsProperties;
+import com.supermap.data.DatasetVector;
+import com.supermap.desktop.properties.CommonProperties;
 import com.supermap.desktop.ui.CheckTableModle;
 import com.supermap.desktop.ui.controls.CheckHeaderCellRenderer;
 
@@ -13,66 +13,56 @@ import java.util.ArrayList;
  */
 public class SmChooseTable extends JTable {
     CheckTableModle checkTableModle = null;
+    private final Object[] tableTitle = {"", CommonProperties.getString("String_Field_Caption")};
     private final static int checkColumnIndex=0;
     private final static int checkColumnIndexMaxSize=40;
     private final static int rowHeight=23;
-
-    public SmChooseTable() {
-        super();
-        init();
-    }
+    private final static int enableColumn=0;
+    private static final int TABLE_COLUMN_CHECKABLE = 0;
+    private static final int TABLE_COLUMN_CAPTION = 1;
+    private DatasetVector datasetVector=null;
 
     private void init(){
+        this.setRowHeight(rowHeight);
         this.getTableHeader().setDefaultRenderer(new CheckHeaderCellRenderer(this,""));
     }
 
-    public SmChooseTable(Object[][] data, Object[] columnNames, int enableColumn) {
-        this.checkTableModle = new CheckTableModle(data, columnNames, enableColumn);
+    public SmChooseTable(DatasetVector datasetVector) {
+        this.datasetVector=datasetVector;
+        this.checkTableModle = new CheckTableModle(getData(datasetVector), tableTitle, enableColumn);
         this.setModel(this.checkTableModle);
         this.getColumn(this.getModel().getColumnName(checkColumnIndex)).setMaxWidth(checkColumnIndexMaxSize);
-        this.setRowHeight(rowHeight);
         init();
     }
 
-
-    public int getSelectedModelRow() {
-        int selectedRows = -1;
-        for (int i = 0; i < SmChooseTable.this.getRowCount(); i++) {
-            if ((boolean) SmChooseTable.this.getValueAt(i, 0)) {
-                selectedRows=i;
-                break;
+    private Object[][] getData(DatasetVector datasetVector){
+        int count = 0;
+        for (int i = 0; i < datasetVector.getFieldInfos().getCount(); i++) {
+            if (!datasetVector.getFieldInfos().get(i).isSystemField()) {
+                count++;
             }
         }
-        return selectedRows;
+        Object data[][]=new Object[count][2];
+        int length = 0;
+        for (int i = 0; i < datasetVector.getFieldInfos().getCount(); i++) {
+            if (!datasetVector.getFieldInfos().get(i).isSystemField()) {
+                data[length][TABLE_COLUMN_CHECKABLE]=false;
+                data[length][TABLE_COLUMN_CAPTION]= datasetVector.getFieldInfos().get(i).getCaption();
+                length++;
+            }
+        }
+        return data;
     }
 
-    /**
-     * @param index 要获取的选中的多个行，对应列的值，当index为0时即是当前选中行的索引
-     * @return
-     */
-    public ArrayList getSelectedModelRows(int index) {
-        ArrayList<Object> selectedRows = new ArrayList<Object>();
-        if (!isIndexValid(index)) {
-            Application.getActiveApplication().getOutput().output(ControlsProperties.getString("String_setVauleIndexIsInValid"));
-        } else {
-            for (int i = 0; i < this.getRowCount(); i++) {
-                if ((boolean) this.getValueAt(i, 0)) {
-                    if (index != 0) {
-                        selectedRows.add(this.getValueAt(i, index));
-                    } else {
-                        selectedRows.add(i);
-                    }
+    public ArrayList getSelectedFieldsName(){
+        ArrayList<String> selectedFieldsName = new ArrayList<String>();
+        for (int i = 0; i < this.getRowCount(); i++) {
+            for (int j = 0; j < this.datasetVector.getFieldInfos().getCount(); j++) {
+                if ((Boolean) this.getValueAt(i, TABLE_COLUMN_CHECKABLE) && this.datasetVector.getFieldInfos().get(j).getCaption().equals(this.getValueAt(i, TABLE_COLUMN_CAPTION))) {
+                    selectedFieldsName.add(this.datasetVector.getFieldInfos().get(j).getName());
                 }
             }
         }
-        return selectedRows;
-    }
-
-    private boolean isIndexValid(int index) {
-        boolean result = true;
-        if (index < 0 || index > this.getColumnCount() - 1) {
-            result = false;
-        }
-        return result;
+        return selectedFieldsName;
     }
 }
