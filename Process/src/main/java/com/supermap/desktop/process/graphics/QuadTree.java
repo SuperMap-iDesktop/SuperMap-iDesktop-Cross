64dad669845ace846587fb90c4be46ace576aa0c
highsad <wuxiaobo@supermap.com>
2017-02-16 16:45:09 +0800

    调整 canvas 的空间信息存储方式。

--- Process/src/main/java/com/supermap/desktop/process/graphics/QuadTree.java
+++ Process/src/main/java/com/supermap/desktop/process/graphics/QuadTree.java
@@ -1,7 +1,10 @@
 package com.supermap.desktop.process.graphics;
 
+import com.supermap.desktop.utilities.ListUtilities;
+
 import java.awt.*;
-import java.util.HashMap;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Vector;
@@ -13,7 +16,10 @@ import java.util.concurrent.ConcurrentHashMap;
  * 采用四叉树结构存储，一个范围 Rectangle 所覆盖的区块均添加该 Rectangle 的管理。
  */
 public class QuadTree<T> {
+	public final static Rectangle ZERO = new Rectangle(0, 0, 0, 0);
+
 	private Vector<T> datas = new Vector<>();
+	private ConcurrentHashMap<T, Rectangle> outsideDatas = new ConcurrentHashMap<>();
 	private QuadNode root;
 	private Rectangle bounds;
 	private double minNodeWidth = 32;
@@ -45,8 +51,12 @@ public class QuadTree<T> {
 	 */
 	public void insert(T data, int index, Rectangle rect) {
 		if (!this.datas.contains(data)) {
-			this.datas.add(index, data);
-			this.root.add(data, bounds);
+			if (!this.bounds.intersects(rect)) {
+				this.outsideDatas.put(data, rect);
+			} else {
+				this.datas.add(index, data);
+				this.root.add(data, bounds);
+			}
 		}
 	}
 
@@ -54,6 +64,35 @@ public class QuadTree<T> {
 		if (this.datas.contains(data)) {
 			this.datas.remove(data);
 			this.root.remove(data);
+		}
+
+		if (this.outsideDatas.containsKey(data)) {
+			this.outsideDatas.remove(data);
+		}
+	}
+
+	public T[] search(Point point) {
+		return this.root.search(point);
+	}
+
+	public Rectangle findBounds(T data) {
+		if (this.outsideDatas.containsKey(data)) {
+			return this.outsideDatas.get(data);
+		} else {
+			return this.root.findBounds(data);
+		}
+	}
+
+	public void reorganize() {
+		ConcurrentHashMap<T, Rectangle> dataMap = new ConcurrentHashMap<>();
+		this.root.getAllDatas(dataMap);
+		this.root.reset();
+		this.root.bounds = (Rectangle) this.bounds.clone();
+
+		Iterator iterator = dataMap.entrySet().iterator();
+		while (iterator.hasNext()) {
+			Map.Entry<T, Rectangle> entry = (Map.Entry<T, Rectangle>) iterator.next();
+			this.root.add(entry.getKey(), entry.getValue());
 		}
 	}
 
@@ -66,8 +105,12 @@ public class QuadTree<T> {
 		private QuadNode southWest;
 		private QuadNode southEast;
 
-		public QuadNode(Rectangle bounds) {
+		public QuadNode() {
 
+		}
+
+		public QuadNode(Rectangle bounds) {
+			this.bounds = bounds;
 		}
 
 		public void add(T data, Rectangle rect) {
@@ -112,6 +155,112 @@ public class QuadTree<T> {
 				this.northEast.remove(data);
 				this.southWest.remove(data);
 				this.southEast.remove(data);
+			}
+		}
+
+		public T[] search(Point point) {
+			if (!this.bounds.contains(point)) {
+				return null;
+			}
+
+			ArrayList<T> list = new ArrayList<>();
+			Iterator iterator = this.datas.entrySet().iterator();
+
+			while (iterator.hasNext()) {
+				Map.Entry<T, Rectangle> entry = (Map.Entry<T, Rectangle>) iterator.next();
+				if (!list.contains(entry.getKey()) && entry.getValue().contains(point)) {
+					list.add(entry.getKey());
+				}
+			}
+
+			if (!this.isLeaf()) {
+				ListUtilities.addArraySingle(list, this.northWest.search(point));
+				ListUtilities.addArraySingle(list, this.northEast.search(point));
+				ListUtilities.addArraySingle(list, this.southWest.search(point));
+				ListUtilities.addArraySingle(list, this.southEast.search(point));
+			}
+			return list.size() == 0 ? null : (T[]) list.toArray(new Object[list.size()]);
+		}
+
+		public Rectangle findBounds(T data) {
+			Rectangle rect = this.datas.get(data);
+
+			if (rect == null) {
+				rect = this.northWest.findBounds(data);
+			}
+
+			if (rect == null) {
+				rect = this.northEast.findBounds(data);
+			}
+
+			if (rect == null) {
+				rect = this.southWest.findBounds(data);
+			}
+
+			if (rect == null) {
+				rect = this.southEast.findBounds(data);
+			}
+			return rect;
+		}
+
+		public Map<T, Rectangle> getDatas() {
+			if (this.datas.size() == 0) {
+				return null;
+			}
+
+			ConcurrentHashMap<T, Rectangle> map = new ConcurrentHashMap<>();
+			map.putAll(this.datas);
+			return map;
+		}
+
+		public void getDatas(Map map) {
+			if (map == null) {
+				return;
+			}
+
+			map.putAll(this.datas);
+		}
+
+		public Map<T, Rectangle> getAllDatas() {
+			if (isLeaf()) {
+				return getDatas();
+			}
+
+			ConcurrentHashMap<T, Rectangle> map = new ConcurrentHashMap<>();
+			map.putAll(this.northWest.getAllDatas());
+			map.putAll(this.northEast.getAllDatas());
+			map.putAll(this.southWest.getAllDatas());
+			map.putAll(this.southEast.getAllDatas());
+			return map;
+		}
+
+		public void getAllDatas(Map map) {
+			if (map == null) {
+				return;
+			}
+
+			if (isLeaf()) {
+				map.putAll(this.datas);
+			} else {
+				this.northWest.getAllDatas(map);
+				this.northEast.getAllDatas(map);
+				this.southWest.getAllDatas(map);
+				this.southEast.getAllDatas(map);
+			}
+		}
+
+		public void reset() {
+			this.datas.clear();
+			this.bounds = ZERO;
+			if (!isLeaf()) {
+				this.northWest.reset();
+				this.northEast.reset();
+				this.southWest.reset();
+				this.southEast.reset();
+				this.northWest = null;
+				this.northEast = null;
+				this.southWest = null;
+				this.southEast = null;
 			}
 		}
 
