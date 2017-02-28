05eaca3e7cea6129723b7bf071ebc02ccbee5c3f
yuanrui <yuanrui@supermap.com>
2017-02-28 12:01:52 +0800

    默认值文本框控件重构 yuanR

--- Controls/src/main/java/com/supermap/desktop/ui/controls/TextFields/DefaultValueTextField.java
+++ Controls/src/main/java/com/supermap/desktop/ui/controls/TextFields/DefaultValueTextField.java
@@ -12,43 +12,116 @@ import java.awt.event.FocusListener;
  *
  * @author YuanR
  *         2017/2/16
- *         对控件进行重构，使其类似于“QQ”登录界面输入账号和密码的文本框--yuanR 2017.2.25
+ *         对控件进行重构，使其类似于“QQ”登录界面输入账号和密码的文本框--yuanR 2017.2.28
  */
 
 public class DefaultValueTextField extends JTextField {
 
 	private ImageIcon searchIcon;
 	private String defaultValue;
-	private String defaultLabelWarning;
+	private String defaultWarningValue;
 
-	/**
-	 * @param text
-	 */
-	public void setDefaultValue(String text) {
-		this.defaultValue = text;
-		this.setText(this.defaultValue);
-		this.setForeground(Color.gray);
-
-	}
-
-	/**
-	 * @param text
-	 */
-	public void setDefaulToolTipText(String text) {
-		if (!StringUtilities.isNullOrEmpty(text)) {
-			this.defaultLabelWarning = text;
-			setToolTipText(this.defaultLabelWarning);
+	@Override
+	public String getText() {
+		if (super.getText().equals(defaultWarningValue)) {
+			return null;
+		} else {
+			return super.getText();
 		}
 	}
 
-//	/**
+	/**
+	 * 获得默认值
+	 *
+	 * @return
+	 */
+	public String getDefaultValue() {
+		return this.defaultValue;
+	}
+
+	/**
+	 * 获得默认提示文本
+	 *
+	 * @return
+	 */
+	public String getDefaulWarningText() {
+		return this.defaultWarningValue;
+	}
+
+	/**
+	 * 默认构造方法
+	 */
+	public DefaultValueTextField() {
+		super();
+	}
+
+
+	/**
+	 * 构造函数
+	 *
+	 * @param Value
+	 * @param defaultWarningValue
+	 */
+	public DefaultValueTextField(String Value, String defaultWarningValue) {
+		super();
+		setDefaultValue(Value);
+		setDefaulWarningText(defaultWarningValue);
+		initListener();
+	}
+
+	/**
+	 * 设置文本框默认值
+	 *
+	 * @param text
+	 */
+	public void setDefaultValue(String text) {
+		if (!StringUtilities.isNullOrEmpty(text)) {
+			this.defaultValue = text;
+			this.setText(this.defaultValue);
+		}
+	}
+
+	/**
+	 * 设置提示文本，当文本框值为空的时候，显示提示信息
+	 *
+	 * @param text
+	 */
+	public void setDefaulWarningText(String text) {
+		if (!StringUtilities.isNullOrEmpty(text)) {
+			this.defaultWarningValue = text;
+		}
+	}
+
+	/**
+	 * 添加监听
+	 */
+	private void initListener() {
+		this.addFocusListener(new FocusListener() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if (getText() == null) {
+					setText("");
+					setForeground(Color.black);
+				}
+			}
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				if (StringUtilities.isNullOrEmptyString(getText())) {
+					setText(defaultWarningValue);
+					setForeground(Color.gray);
+				}
+			}
+		});
+	}
+	//	/**
 //	 * @param g
 //	 */
 //	@Override
 //	public void paintComponent(Graphics g) {
 //		super.paintComponent(g);
 //		// 当有提示信息时，显示提示图标
-//		if (!StringUtilities.isNullOrEmpty(defaultLabelWarning)) {
+//		if (!StringUtilities.isNullOrEmpty(defaultWarningValue)) {
 //			super.paintComponent(g);
 ////			int iconWidth = this.searchIcon.getIconWidth();
 //			int iconHeight = this.searchIcon.getIconHeight();
@@ -60,64 +133,4 @@ public class DefaultValueTextField extends JTextField {
 //		}
 //		repaint();
 //	}
-
-	/**
-	 * 默认构造方法
-	 */
-	public DefaultValueTextField() {
-		this("", "");
-	}
-
-	/**
-	 * 构造方法
-	 */
-	public DefaultValueTextField(String defaultValue) {
-		this(defaultValue, null);
-	}
-
-	/**
-	 * 构造函数
-	 *
-	 * @param Value
-	 * @param LabelWarning
-	 */
-	public DefaultValueTextField(String Value, String LabelWarning) {
-		super();
-		this.defaultValue = Value;
-		this.defaultLabelWarning = LabelWarning;
-//		this.searchIcon = (ImageIcon) ControlsResources.getIcon("/controlsresources/Image_Property.png");
-
-		initComponents();
-		initListener();
-	}
-
-	/**
-	 * 初始化控件属性
-	 */
-	private void initComponents() {
-		this.setText(this.defaultValue);
-		// 当有提示信息时再设置其ToolTipText
-		if (!StringUtilities.isNullOrEmpty(this.defaultLabelWarning)) {
-			this.setToolTipText(this.defaultLabelWarning);
-		}
-		this.setForeground(Color.gray);
-	}
-
-	private void initListener() {
-		this.addFocusListener(new FocusListener() {
-			@Override
-			public void focusGained(FocusEvent e) {
-				setText("");
-				setForeground(Color.black);
-			}
-
-			@Override
-			public void focusLost(FocusEvent e) {
-				if (StringUtilities.isNullOrEmptyString(getText())) {
-					setText(defaultValue);
-					setForeground(Color.gray);
-				}
-			}
-		});
-	}
 }
