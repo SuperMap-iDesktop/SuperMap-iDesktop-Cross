b6052f38b5491d2abaf2150da9b347d1df6c2ae1
yuanrui <yuanrui@supermap.com>
2017-03-01 15:55:19 +0800

    yuanR

--- Controls/src/main/java/com/supermap/desktop/ui/controls/CellRenders/ListDataCellRender.java
+++ Controls/src/main/java/com/supermap/desktop/ui/controls/CellRenders/ListDataCellRender.java
@@ -1,12 +1,13 @@
 package com.supermap.desktop.ui.controls.CellRenders;
 
 import com.supermap.desktop.ui.controls.DataCell;
+
 import javax.swing.*;
 import java.awt.*;
 
 /**
  * @author YuanR 17.2.15
- * 构造ListCellRenderer渲染器，主要适用于DatasetCombox和datasourceComboBox
+ *         构造ListCellRenderer渲染器，主要适用于DatasetCombox和datasourceComboBox
  */
 public class ListDataCellRender extends DefaultListCellRenderer {
 
@@ -20,10 +21,10 @@ public class ListDataCellRender extends DefaultListCellRenderer {
 			result = new DataCell(value);
 		}
 		if (isSelected) {
-			result.setBackground(list.getSelectionBackground());
-			result.setForeground(list.getSelectionForeground());
+			result.setBackground(new Color(51, 153, 255));
+			result.setForeground(Color.white);
 		} else {
-			result.setBackground(list.getBackground());
+			result.setBackground(Color.white);
 		}
 		return result;
 	}
