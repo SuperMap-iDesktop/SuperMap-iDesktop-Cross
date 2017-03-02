99396df97a32455193fe61f05dcb65d75b8aa67f
highsad <wuxiaobo@supermap.com>
2017-03-02 20:20:49 +0800

    解决错误。

--- Process/src/main/java/com/supermap/desktop/process/graphics/interaction/MultiSelction.java
+++ Process/src/main/java/com/supermap/desktop/process/graphics/interaction/MultiSelction.java
@@ -1,7 +1,12 @@
 package com.supermap.desktop.process.graphics.interaction;
 
+import com.supermap.desktop.process.graphics.GraphCanvas;
+
 /**
  * Created by highsad on 2017/3/2.
  */
 public class MultiSelction extends Selection {
+	public MultiSelction(GraphCanvas canvas) {
+		super(canvas);
+	}
 }
