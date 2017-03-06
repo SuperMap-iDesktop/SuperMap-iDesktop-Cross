32b2896f847fa5c782dcc6590063712f7b23b286
yuanrui <yuanrui@supermap.com>
2017-03-06 14:14:37 +0800

    缓冲区功能优化 yunaR

--- Controls/src/main/java/com/supermap/desktop/controls/ControlDefaultValues.java
+++ Controls/src/main/java/com/supermap/desktop/controls/ControlDefaultValues.java
@@ -11,7 +11,7 @@ public class ControlDefaultValues {
 	//确定取消按钮间隔值
 	public static final int DEFAULT_PREFERREDSIZE_GAP = 10;
 	// 面板之间间距
-	public static final int DEFAULT_PANEL_GAP =5;
+	public static final int DEFAULT_PANEL_GAP = 5;
 	// 设置缓冲区面板控件尺寸
 	public static final Dimension BUFFERCOMPONT_PREFERREDSIZE = new Dimension(80, 20);
 
