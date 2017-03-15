247be1fe2f6bcf3a6264669b69c522c4b10255fb
xiexiuji <xiexiuji@supermap.com>
2017-03-15 15:01:18 +0800

    矩阵类初步实现。

---
+++ Process/src/main/java/com/supermap/desktop/process/core/NodeException.java
@@ -0,0 +1,14 @@
+package com.supermap.desktop.process.core;
+
+/**
+ * Created by xie on 2017/3/14.
+ */
+public class NodeException extends Exception {
+    public NodeException() {
+        super();
+    }
+
+    public NodeException(String message) {
+        super(message);
+    }
+}
