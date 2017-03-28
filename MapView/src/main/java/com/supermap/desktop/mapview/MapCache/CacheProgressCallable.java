13d1cb917e6c85cd395329fc4f8616f7f0323371
lixiaoyao <liwenfa@supermap.com>
2017-03-28 08:52:21 +0800

    地图缓存功能实现，目前不支持导入导出以及多进程和mongoDb多时态，其他的都已经实现了——李文发

---
+++ MapView/src/main/java/com/supermap/desktop/mapview/MapCache/CacheProgressCallable.java
@@ -0,0 +1,52 @@
+package com.supermap.desktop.mapview.MapCache;
+
+import com.supermap.data.SteppedEvent;
+import com.supermap.data.SteppedListener;
+import com.supermap.data.processing.MapCacheBuilder;
+import com.supermap.desktop.Application;
+import com.supermap.desktop.progress.Interface.UpdateProgressCallable;
+
+import java.util.concurrent.CancellationException;
+
+/**
+ * Created by lixiaoyao on 2017/3/23.
+ */
+public class CacheProgressCallable extends UpdateProgressCallable{
+    private MapCacheBuilder mapCacheBuilder;
+    private boolean result;
+
+    private SteppedListener steppedListener = new SteppedListener() {
+
+        @Override
+        public void stepped(SteppedEvent arg0) {
+            try {
+                updateProgress(arg0.getPercent(), String.valueOf(arg0.getRemainTime()), arg0.getMessage());
+            } catch (CancellationException e) {
+                arg0.setCancel(true);
+            }
+        }
+    };
+
+    public CacheProgressCallable(MapCacheBuilder mapCacheBuilder){
+        this.mapCacheBuilder=mapCacheBuilder;
+    }
+
+    @Override
+    public Boolean call() throws Exception {
+        this.result=true;
+        try {
+            this.mapCacheBuilder.addSteppedListener(this.steppedListener);
+            this.result=this.mapCacheBuilder.build();
+        }catch (Exception ex){
+            this.result=false;
+            Application.getActiveApplication().getOutput().output(ex);
+        }finally {
+            return result;
+        }
+    }
+
+    public boolean getResult(){
+        return this.result;
+    }
+
+}
