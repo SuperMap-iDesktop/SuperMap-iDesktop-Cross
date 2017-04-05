97ff29634a3f848fcbd93f72f54d15726901e74f
lixiaoyao <liwenfa@supermap.com>
2017-04-05 17:23:50 +0800

    地图缓存缺陷回归以及封装的PanelGroupBoxViewBounds优化——李文发

---
+++ MapView/src/main/java/com/supermap/desktop/mapview/map/propertycontrols/SelectObjectListener.java
@@ -0,0 +1,8 @@
+package com.supermap.desktop.mapview.map.propertycontrols;
+
+/**
+ * Created by lixiaoyao on 2017/4/5.
+ */
+public abstract class SelectObjectListener {
+    public abstract void selectObjectListener();
+}
