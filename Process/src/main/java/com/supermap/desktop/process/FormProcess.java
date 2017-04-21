adbfa1a15f155d030c975752a457c9e8e0961018
xiajt <xiajintao@supermap.com>
2017-04-21 15:19:02 +0800

    未知缺陷

--- Process/src/main/java/com/supermap/desktop/process/FormProcess.java
+++ Process/src/main/java/com/supermap/desktop/process/FormProcess.java
@@ -34,6 +34,7 @@ import com.supermap.desktop.process.graphics.graphs.RectangleGraph;
 import com.supermap.desktop.process.graphics.interaction.canvas.Selection;
 import com.supermap.desktop.process.graphics.storage.IGraphConnection;
 import com.supermap.desktop.process.graphics.storage.IGraphStorage;
+import com.supermap.desktop.process.meta.MetaProcess;
 import com.supermap.desktop.process.parameter.interfaces.datas.OutputData;
 import com.supermap.desktop.ui.FormBaseChild;
 import com.supermap.desktop.ui.UICommonToolkit;
@@ -418,7 +419,11 @@ public class FormProcess extends FormBaseChild implements IFormProcess {
 					try {
 						Object transferData = transferable.getTransferData(currentDataFlavor);
 						if (transferData instanceof String) {
-							ProcessGraph graph = new ProcessGraph(getCanvas(), WorkflowParser.getMetaProcess((String) transferData));
+							MetaProcess metaProcess = WorkflowParser.getMetaProcess((String) transferData);
+							if (metaProcess == null) {
+								continue;
+							}
+							ProcessGraph graph = new ProcessGraph(getCanvas(), metaProcess);
 							Point location = dtde.getLocation();
 							location = new Point(location.x - graph.getWidth() / 2, location.y - graph.getHeight() / 2);
 							Point inverse = getCanvas().getCoordinateTransform().inverse(location);
