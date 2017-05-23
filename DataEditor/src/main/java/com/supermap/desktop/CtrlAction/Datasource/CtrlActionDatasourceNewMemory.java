4fdb6cff435aae4c425836e2b3db278291e99e5c
xiajt <xiajintao@supermap.com>
2017-05-23 10:05:24 +0800

    缺陷UGDJ-592

--- DataEditor/src/main/java/com/supermap/desktop/CtrlAction/Datasource/CtrlActionDatasourceNewMemory.java
+++ DataEditor/src/main/java/com/supermap/desktop/CtrlAction/Datasource/CtrlActionDatasourceNewMemory.java
@@ -1,9 +1,14 @@
 package com.supermap.desktop.CtrlAction.Datasource;
 
+import com.supermap.data.Datasource;
+import com.supermap.data.DatasourceConnectionInfo;
+import com.supermap.data.Datasources;
+import com.supermap.data.EngineType;
 import com.supermap.desktop.Application;
 import com.supermap.desktop.Interface.IBaseItem;
 import com.supermap.desktop.Interface.IForm;
 import com.supermap.desktop.implement.CtrlAction;
+import com.supermap.desktop.utilities.DatasourceUtilities;
 
 public class CtrlActionDatasourceNewMemory extends CtrlAction {
 
@@ -14,6 +19,16 @@ public class CtrlActionDatasourceNewMemory extends CtrlAction {
 	@Override
 	public void run() {
 		try {
+			Datasources datasources = Application.getActiveApplication().getWorkspace().getDatasources();
+			String datasourceAlias = DatasourceUtilities.getAvailableDatasourceAlias("MemoryDatasource", 0);
+			DatasourceConnectionInfo info = new DatasourceConnectionInfo();
+			info.setEngineType(EngineType.MEMORY);
+			info.setAlias(datasourceAlias);
+			Datasource datasource = datasources.create(info);
+			if (datasource != null) {
+//				UICommonToolkit.getWorkspaceTreeDatasourceNode();
+			}
+
 			JDialogDatasourceNewMemory dialog = new JDialogDatasourceNewMemory();
 			dialog.setVisible(true);			
 		} catch (Exception ex) {
