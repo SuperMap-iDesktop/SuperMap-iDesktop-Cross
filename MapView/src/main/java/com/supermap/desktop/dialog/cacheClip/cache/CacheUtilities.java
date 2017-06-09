a0db6f8dea1dc6777d1172c3dcc9b6eddc30812e
xiexiuji <xiexiuji@supermap.com>
2017-06-09 15:20:20 +0800

    linux上执行多进程切图功能修改

--- MapView/src/main/java/com/supermap/desktop/dialog/cacheClip/cache/CacheUtilities.java
+++ MapView/src/main/java/com/supermap/desktop/dialog/cacheClip/cache/CacheUtilities.java
@@ -2,6 +2,7 @@ package com.supermap.desktop.dialog.cacheClip.cache;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -42,29 +43,36 @@ public class CacheUtilities {
 	 * @param cacheType
 	 */
 	public static void startProcess(String[] params, String className, String cacheType) {
-		ArrayList<String> arguments = new ArrayList<String>();
-		arguments.add("java");
-		//arguments.addAll(jvmArgs);
-		arguments.add("-cp");
-		String projectPath = replacePath(System.getProperty("user.dir"));
+		try {
+			ArrayList<String> arguments = new ArrayList<String>();
+			arguments.add("java");
+			//arguments.addAll(jvmArgs);
+			arguments.add("-cp");
+			String projectPath = replacePath(System.getProperty("user.dir"));
 
 //		String jarPath = ".;" + projectPath + "\\bin\\com.supermap.data.jar;" + projectPath + "\\bin\\com.supermap.mapping.jar;" + projectPath + "\\bin\\com.supermap.tilestorage.jar;" + projectPath + "\\bin\\com.supermap.data.processing.jar;" + projectPath + "\\bundles\\idesktop_bundles\\MapView.jar";
-		String jarPath = "";
-		if (isWindows()){
-			jarPath = ".;" + projectPath + "\\bin\\com.supermap.data.jar;" + projectPath + "\\bin\\com.supermap.mapping.jar;" + projectPath + "\\bin\\com.supermap.tilestorage.jar;" + projectPath + "\\bin\\com.supermap.data.processing.jar;" + projectPath + "\\bundles\\idesktop_bundles\\MapView.jar";
-		}else{
-			jarPath = ".;" + projectPath + "/bin/com.supermap.data.jar;" + projectPath + "/bin/com.supermap.mapping.jar;" + projectPath + "/bin/com.supermap.tilestorage.jar;" + projectPath + "/bin/com.supermap.data.processing.jar;" + projectPath + "/bundles/idesktop_bundles/MapView.jar";
-		}
+			String jarPath = "";
+			if (isWindows()) {
+				jarPath = ".;" + projectPath + "\\bin\\com.supermap.data.jar;" + projectPath + "\\bin\\com.supermap.mapping.jar;" + projectPath + "\\bin\\com.supermap.tilestorage.jar;" + projectPath + "\\bin\\com.supermap.data.processing.jar;" + projectPath + "\\bundles\\idesktop_bundles\\MapView.jar";
+			} else {
+				//直接运行时必须依赖主键，动态添加LD_LIBRARY_PATH
+				String[] commonds = {"sh", "-c", "export LD_LIBRARY_PATH=" + projectPath + "/bin" + ";export PATH=$LD_LIBRARY_PATH:$PATH"};
+				Runtime.getRuntime().exec(commonds);
+				jarPath = projectPath + "/bin/com.supermap.data.jar:" + projectPath + "/bin/com.supermap.mapping.jar:" + projectPath + "/bin/com.supermap.tilestorage.jar:" + projectPath + "/bin/com.supermap.data.processing.jar:" + projectPath + "/bundles/idesktop_bundles/MapView.jar: ";
+			}
 //		String jarPath = ".;" + projectPath + "\\bundles\\require_bundles\\Core.jar;" + projectPath + "\\bundles\\idesktop_bundles\\MapView.jar";
-		arguments.add(jarPath);
-		arguments.add(className);
-		for (int i = 0; i < params.length; i++) {
-			arguments.add(params[i]);
+			arguments.add(jarPath);
+			arguments.add(className);
+			for (int i = 0; i < params.length; i++) {
+				arguments.add(params[i]);
+			}
+			ProcessManager manager = ProcessManager.getInstance();
+			SubprocessThread thread = new SubprocessThread(arguments, cacheType);
+			manager.addProcess(thread);
+			thread.start();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		ProcessManager manager = ProcessManager.getInstance();
-		SubprocessThread thread = new SubprocessThread(arguments, cacheType);
-		manager.addProcess(thread);
-		thread.start();
 	}
 
 	public static boolean isWindows() {
