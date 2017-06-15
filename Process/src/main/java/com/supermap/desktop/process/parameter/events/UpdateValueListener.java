38fd3e602eb4d4a1f53d984cb2d6fcfcc32c6c93
xiajt <xiajintao@supermap.com>
2017-06-15 15:14:33 +0800

    事件的实现修改

--- Process/src/main/java/com/supermap/desktop/process/parameter/events/UpdateValueListener.java
+++ Process/src/main/java/com/supermap/desktop/process/parameter/events/UpdateValueListener.java
@@ -3,6 +3,7 @@ package com.supermap.desktop.process.parameter.events;
 /**
  * Created by hanyz on 2017/5/9.
  */
+// 会导致数据不能及时更新，与其他控件的联动失效
 public interface UpdateValueListener {
 	void fireUpdateValue(ParameterUpdateValueEvent event);
 }
