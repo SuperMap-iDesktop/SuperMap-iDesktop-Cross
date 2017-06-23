d1c3abb81e32200e7e93df262893b5d52d645d4d
highsad <wuxiaobo@supermap.com>
2017-06-23 15:21:43 +0800

    1. 图层管理器右键菜单新增 插入数据 功能； 2. 地图窗口右键菜单新增 添加数据 功能。

---
+++ MapView/src/main/java/com/supermap/desktop/CtrlAction/CtrlActionAddDataset.java
@@ -0,0 +1,77 @@
+package com.supermap.desktop.CtrlAction;
+
+import com.supermap.data.Dataset;
+import com.supermap.data.DatasetType;
+import com.supermap.desktop.Application;
+import com.supermap.desktop.Interface.IBaseItem;
+import com.supermap.desktop.Interface.IForm;
+import com.supermap.desktop.Interface.IFormMap;
+import com.supermap.desktop.implement.CtrlAction;
+import com.supermap.desktop.ui.controls.DialogResult;
+import com.supermap.desktop.ui.controls.datasetChoose.DatasetChooser;
+import com.supermap.desktop.utilities.MapUtilities;
+import com.supermap.mapping.Layer;
+
+import java.util.List;
+
+/**
+ * Created by highsad on 2017/6/23.
+ */
+public class CtrlActionAddDataset extends CtrlAction {
+
+	public CtrlActionAddDataset(IBaseItem caller, IForm formClass) {
+		super(caller, formClass);
+	}
+
+	@Override
+	public void run() {
+		try {
+
+			IFormMap formMap = (IFormMap) Application.getActiveApplication().getActiveForm();
+			if (formMap != null) {
+				DatasetType[] datasetTypes = new DatasetType[]{
+						DatasetType.POINT, DatasetType.LINE, DatasetType.REGION, DatasetType.TEXT, DatasetType.CAD, DatasetType.NETWORK,
+						DatasetType.LINEM, DatasetType.GRID, DatasetType.IMAGE, DatasetType.POINT3D, DatasetType.LINE3D, DatasetType.REGION3D,
+						DatasetType.GRIDCOLLECTION, DatasetType.IMAGECOLLECTION, DatasetType.PARAMETRICLINE, DatasetType.PARAMETRICREGION,
+						DatasetType.NETWORK3D
+				};
+				DatasetChooser datasetChooser = new DatasetChooser();
+				datasetChooser.setSupportDatasetTypes(datasetTypes);
+				if (datasetChooser.showDialog() == DialogResult.OK) {
+					List<Dataset> datasetsToMap = datasetChooser.getSelectedDatasets();
+					Layer[] activeLayers = formMap.getActiveLayers();
+
+					if (activeLayers == null || activeLayers.length == 0) {
+						return;
+					}
+
+					Layer[] insertLayers = new Layer[datasetsToMap.size()];
+					for (int i = datasetsToMap.size() - 1; i >= 0; i--) {
+						insertLayers[i] = MapUtilities.addDatasetToMap(formMap.getMapControl().getMap(), datasetsToMap.get(i), true);
+					}
+
+					formMap.setActiveLayers(insertLayers);
+				}
+			}
+		} catch (Exception ex) {
+			Application.getActiveApplication().getOutput().output(ex);
+		}
+	}
+
+	@Override
+	public boolean enable() {
+
+		boolean enable = false;
+
+		try {
+			if (Application.getActiveApplication().getWorkspace().getDatasources().getCount() > 0
+					&& null != Application.getActiveApplication().getActiveForm() && Application.getActiveApplication().getActiveForm() instanceof IFormMap) {
+				enable = true;
+			}
+		} catch (Exception ex) {
+			Application.getActiveApplication().getOutput().output(ex);
+		}
+		return enable;
+	}
+
+}
