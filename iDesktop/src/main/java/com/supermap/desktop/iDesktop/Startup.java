324241b959fa7f7a132eb4388858c09a585741e1
xiajt <xiajintao@supermap.com>
2017-06-23 10:13:30 +0800

    删除无用代码

--- iDesktop/src/main/java/com/supermap/desktop/iDesktop/Startup.java
+++ iDesktop/src/main/java/com/supermap/desktop/iDesktop/Startup.java
@@ -22,9 +22,6 @@ public class Startup {
 //			if(UIManager.getLookAndFeel() instanceof MetalLookAndFeel){
 //				((MetalLookAndFeel)UIManager.getLookAndFeel()).setCurrentTheme(new OceanTheme());
 //			}
-			args = new String[]{
-					"C:\\Users\\Administrator\\Desktop\\2.smwu"
-			};
 			JSONArray jsonArray = new JSONArray();
 			Collections.addAll(jsonArray, args);
 			System.setProperty("DesktopCrossStartArgs", jsonArray.toJSONString());
