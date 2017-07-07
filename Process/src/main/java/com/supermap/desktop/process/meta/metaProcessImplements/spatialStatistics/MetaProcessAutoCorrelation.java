0f55238120f081ac4cda84f4ac8ab61fcc8cad4b
yuanR <yuanrui@supermap.com>
2017-07-07 10:40:36 +0800

    平均最近邻功能结果输出优化 yuanR

--- Process/src/main/java/com/supermap/desktop/process/meta/metaProcessImplements/spatialStatistics/MetaProcessAutoCorrelation.java
+++ Process/src/main/java/com/supermap/desktop/process/meta/metaProcessImplements/spatialStatistics/MetaProcessAutoCorrelation.java
@@ -10,6 +10,7 @@ import com.supermap.desktop.process.parameter.implement.ParameterTextArea;
 
 /**
  * @author XiaJT
+ * 空间自相关分析
  */
 public class MetaProcessAutoCorrelation extends MetaProcessAnalyzingPatterns {
 
