17779b59b1872177f0fb5f11e0cbc22e7cfa1ca6
yuanR <yuanrui@supermap.com>
2017-07-19 15:05:51 +0800

    用户大会展示入口实现 yuanR

---
+++ Process/src/main/java/com/supermap/desktop/process/ctrlAction/CtrlActionDEMO.java
@@ -0,0 +1,46 @@
+package com.supermap.desktop.process.ctrlAction;
+
+import com.supermap.desktop.Application;
+import com.supermap.desktop.Interface.IBaseItem;
+import com.supermap.desktop.Interface.IForm;
+import com.supermap.desktop.Interface.IFormManager;
+import com.supermap.desktop.implement.CtrlAction;
+import com.supermap.desktop.process.demo.Demo;
+import com.supermap.desktop.ui.FormBaseChild;
+
+/**
+ * Created by yuanR on 2017/7/19  .
+ * 用户大会展示功能入口
+ */
+public class CtrlActionDEMO extends CtrlAction {
+
+	public CtrlActionDEMO(IBaseItem caller, IForm formClass) {
+		super(caller, formClass);
+	}
+
+	FormBaseChild workspaceManagerWindow;
+	IFormManager formManager;
+
+	public void run() {
+		try {
+			formManager = Application.getActiveApplication().getMainFrame().getFormManager();
+			if (workspaceManagerWindow == null) {
+				workspaceManagerWindow = new Demo();
+				formManager.showChildForm(workspaceManagerWindow);
+			} else {
+				if (!workspaceManagerWindow.isShowing()) {
+					formManager.showChildForm(workspaceManagerWindow);
+				} else {
+					formManager.close(workspaceManagerWindow);
+				}
+			}
+		} catch (Exception ex) {
+			Application.getActiveApplication().getOutput().output(ex);
+		}
+	}
+
+	@Override
+	public boolean enable() {
+		return true;
+	}
+}
