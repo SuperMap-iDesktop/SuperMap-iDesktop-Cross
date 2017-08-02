cd4ff41b63615ef6ff7b8e894964b3300e6dcf96
xiajt <xiajintao@supermap.com>
2017-08-02 18:18:48 +0800

    演示程序

--- UserConferenceDemo/src/main/java/com/supermap/desktop/userConferenceDemo/demo/CtrlActionDEMO.java
+++ UserConferenceDemo/src/main/java/com/supermap/desktop/userConferenceDemo/demo/CtrlActionDEMO.java
@@ -2,10 +2,12 @@ package com.supermap.desktop.userConferenceDemo.demo;
 
 import com.supermap.desktop.Application;
 import com.supermap.desktop.Interface.IBaseItem;
+import com.supermap.desktop.Interface.IDockbar;
 import com.supermap.desktop.Interface.IForm;
 import com.supermap.desktop.Interface.IFormManager;
 import com.supermap.desktop.implement.CtrlAction;
 import com.supermap.desktop.ui.FormBaseChild;
+import com.supermap.desktop.ui.controls.DockbarManager;
 
 /**
  * Created by yuanR on 2017/7/19  .
@@ -22,6 +24,15 @@ public class CtrlActionDEMO extends CtrlAction {
 
 	public void run() {
 		try {
+			IDockbar workspaceTreeDockbar = ((DockbarManager) Application.getActiveApplication().getMainFrame().getDockbarManager()).getWorkspaceComponentManager();
+			IDockbar layersComponentManager = ((DockbarManager) Application.getActiveApplication().getMainFrame().getDockbarManager()).getLayersComponentManager();
+			if (workspaceTreeDockbar != null) {
+				workspaceTreeDockbar.setVisible(false);
+			}
+			if (layersComponentManager != null) {
+				layersComponentManager.setVisible(false);
+			}
+
 			formManager = Application.getActiveApplication().getMainFrame().getFormManager();
 			if (workspaceManagerWindow == null) {
 				workspaceManagerWindow = new Demo();
