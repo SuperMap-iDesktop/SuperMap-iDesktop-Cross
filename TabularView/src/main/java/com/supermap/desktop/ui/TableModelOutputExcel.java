2fdd87fab1846af6e58855e649a84031f8e8f303
xiajt <xiajintao@supermap.com>
2017-08-08 19:45:53 +0800

    导出表

---
+++ TabularView/src/main/java/com/supermap/desktop/ui/TableModelOutputExcel.java
@@ -0,0 +1,39 @@
+package com.supermap.desktop.ui;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * @author XiaJT
+ */
+public class TableModelOutputExcel extends DefaultTableModel {
+
+	@Override
+	public int getRowCount() {
+		return super.getRowCount();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return super.getColumnCount();
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		return super.getColumnName(column);
+	}
+
+	@Override
+	public boolean isCellEditable(int row, int column) {
+		return super.isCellEditable(row, column);
+	}
+
+	@Override
+	public Object getValueAt(int row, int column) {
+		return super.getValueAt(row, column);
+	}
+
+	@Override
+	public void setValueAt(Object aValue, int row, int column) {
+		super.setValueAt(aValue, row, column);
+	}
+}
