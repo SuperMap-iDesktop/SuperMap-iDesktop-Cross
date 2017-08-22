81cdd86b1046c09263a3ee5f68af1494b3e05db6
xiajt <xiajintao@supermap.com>
2017-08-22 09:45:44 +0800

    meta缺陷

---
+++ WorkflowView/src/main/java/com/supermap/desktop/WorkflowView/meta/metaProcessImplements/MetaProcessGridAnalystSetting.java
@@ -0,0 +1,30 @@
+package com.supermap.desktop.WorkflowView.meta.metaProcessImplements;
+
+import com.supermap.desktop.WorkflowView.meta.MetaProcess;
+
+/**
+ * @author XiaJT
+ */
+public class MetaProcessGridAnalystSetting extends MetaProcess {
+
+	private boolean isChanged = false;
+
+	public MetaProcessGridAnalystSetting() {
+
+	}
+
+	@Override
+	public String getTitle() {
+		return null;
+	}
+
+	@Override
+	public boolean execute() {
+		return false;
+	}
+
+	@Override
+	public String getKey() {
+		return null;
+	}
+}
