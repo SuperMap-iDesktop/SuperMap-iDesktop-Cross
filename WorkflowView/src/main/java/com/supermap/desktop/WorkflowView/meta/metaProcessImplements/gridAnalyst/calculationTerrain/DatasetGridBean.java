567d49bce720646c212b2a7ba9e673e644f75dce
yuanR <yuanrui@supermap.com>
2017-08-31 13:54:55 +0800

    生成正射三维影像功能实现，优化功能中颜色表的设置 yuanR

---
+++ WorkflowView/src/main/java/com/supermap/desktop/WorkflowView/meta/metaProcessImplements/gridAnalyst/calculationTerrain/DatasetGridBean.java
@@ -0,0 +1,62 @@
+package com.supermap.desktop.WorkflowView.meta.metaProcessImplements.gridAnalyst.calculationTerrain;
+
+import com.supermap.data.ColorDictionary;
+import com.supermap.data.DatasetGrid;
+import com.supermap.mapping.LayerSettingGrid;
+
+/**
+ * Created by yuanR on 2017/8/31 0031.
+ * 栅格数据集相关属性管理类
+ * 目前主要负责LayerSettingGrid和originKeys属性的管理
+ */
+public class DatasetGridBean {
+
+	private DatasetGrid datasetGrid;
+
+	public DatasetGrid getDatasetGrid() {
+		return datasetGrid;
+	}
+
+	public LayerSettingGrid getLayerSettingGrid() {
+		return layerSettingGrid;
+	}
+
+	public void setLayerSettingGrid(LayerSettingGrid layerSettingGrid) {
+		this.layerSettingGrid = layerSettingGrid;
+	}
+
+	public double[] getOriginKeys() {
+		return originKeys;
+	}
+
+	public void setOriginKeys(double[] originKeys) {
+		this.originKeys = originKeys;
+	}
+
+	private LayerSettingGrid layerSettingGrid;
+	private double[] originKeys;
+
+	public DatasetGridBean(DatasetGrid datasetGrid) {
+		this.datasetGrid = datasetGrid;
+		init();
+	}
+
+	private void init() {
+		// originKeys
+		int colorLegth = this.datasetGrid.getColorTable().getCount();
+		double minValue = this.datasetGrid.getMinValue();
+		double maxValue = this.datasetGrid.getMaxValue();
+		this.originKeys = new double[colorLegth];
+		double valueGap = (maxValue - minValue) / (colorLegth - 1);
+		for (int i = 0; i < colorLegth; i++) {
+			this.originKeys[i] = minValue + valueGap * i;
+		}
+		// LayerSettingGrid
+		ColorDictionary colorDictionary = new ColorDictionary();
+		for (int i = 0; i < this.datasetGrid.getColorTable().getCount(); i++) {
+			colorDictionary.setColor(this.originKeys[i], this.datasetGrid.getColorTable().get(i));
+		}
+		this.layerSettingGrid = new LayerSettingGrid();
+		this.layerSettingGrid.setColorDictionary(colorDictionary);
+	}
+}
