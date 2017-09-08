9496ae10cd704283d165377ec84b6cbd379568df
highsad <wuxiaobo@supermap.com>
2017-09-08 11:11:33 +0800

    调整在线分析的进度条。

--- WorkflowView/src/main/java/com/supermap/desktop/WorkflowView/meta/metaProcessImplements/MetaProcessPolygonAggregation.java
+++ WorkflowView/src/main/java/com/supermap/desktop/WorkflowView/meta/metaProcessImplements/MetaProcessPolygonAggregation.java
@@ -121,9 +121,9 @@ public class MetaProcessPolygonAggregation extends MetaProcess {
 
 	@Override
 	public boolean execute() {
-		boolean isSuccess;
+		boolean isSuccessful;
 		try {
-			fireRunning(new RunningEvent(this, 0, "start"));
+			fireRunning(new RunningEvent(this, ProcessProperties.getString("String_Running")));
 			IServerService service = parameterIServerLogin.login();
 			CommonSettingCombine input = new CommonSettingCombine("input", "");
 			parameterInputDataType.initSourceInput(input);
@@ -142,42 +142,13 @@ public class MetaProcessPolygonAggregation extends MetaProcess {
 			JobResultResponse response = service.queryResult(MetaKeys.POLYGON_AGGREGATION, commonSettingCombine.getFinalJSon());
 			CursorUtilities.setWaitCursor();
 			if (null != response) {
-				NewMessageBus messageBus = new NewMessageBus(response, new IUpdateProgress() {
-					@Override
-					public boolean isCancel() {
-						return false;
-					}
-
-					@Override
-					public void setCancel(boolean isCancel) {
-
-					}
-
-					@Override
-					public void updateProgress(int percent, String remainTime, String message) throws CancellationException {
-						fireRunning(new RunningEvent(MetaProcessPolygonAggregation.this, percent, message));
-					}
-
-					@Override
-					public void updateProgress(String message, int percent, String currentMessage) throws CancellationException {
-
-					}
-
-					@Override
-					public void updateProgress(int percent, int totalPercent, String remainTime, String message) throws CancellationException {
-
-					}
-
-					@Override
-					public void updateProgress(int percent, String recentTask, int totalPercent, String message) throws CancellationException {
-
-					}
-				}, DefaultOpenServerMap.INSTANCE);
-				isSuccess = messageBus.run();
+				NewMessageBus messageBus = new NewMessageBus(response, DefaultOpenServerMap.INSTANCE);
+				isSuccessful = messageBus.run();
 			} else {
-				fireRunning(new RunningEvent(this, 100, "Failed"));
-				isSuccess = false;
+				isSuccessful = false;
 			}
+
+			fireRunning(new RunningEvent(this, 100, CoreProperties.getString(isSuccessful ? "String_Message_Succeed" : "String_Message_Failed")));
 			parameters.getOutputs().getData("PolygonAggregationResult").setValue("");// TODO: 2017/6/26 也许没结果,but
 		} catch (Exception e) {
 			Application.getActiveApplication().getOutput().output(e);
@@ -185,7 +156,7 @@ public class MetaProcessPolygonAggregation extends MetaProcess {
 		} finally {
 			CursorUtilities.setDefaultCursor();
 		}
-		return isSuccess;
+		return isSuccessful;
 	}
 
 	@Override
