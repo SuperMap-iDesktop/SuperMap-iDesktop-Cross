ba66065f868dc5d0e2f287acd5a5c00fe139d1e8
lixiaoyao <liwenfa@supermap.com>
2017-09-11 16:14:00 +0800

    复制工作空间和数据源路径缺陷修改—李文发

---
+++ DataView/src/main/java/com/supermap/desktop/CtrlAction/property/CtrlActionCopyPathDatasource.java
@@ -0,0 +1,45 @@
+package com.supermap.desktop.CtrlAction.property;
+
+import com.supermap.data.Datasource;
+import com.supermap.desktop.Application;
+import com.supermap.desktop.Interface.IBaseItem;
+import com.supermap.desktop.Interface.IForm;
+import com.supermap.desktop.dataview.DataViewProperties;
+import com.supermap.desktop.implement.CtrlAction;
+import com.supermap.desktop.ui.UICommonToolkit;
+import com.supermap.desktop.ui.controls.NodeDataType;
+import com.supermap.desktop.ui.controls.TreeNodeData;
+import com.supermap.desktop.ui.controls.WorkspaceTree;
+import com.supermap.desktop.utilities.ClipBoardUtilties;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
+
+/**
+ * Created by lixiaoyao on 2017/9/11.
+ */
+public class CtrlActionCopyPathDatasource extends CtrlAction {
+	public CtrlActionCopyPathDatasource(IBaseItem caller, IForm formClass) {
+		super(caller, formClass);
+	}
+
+	@Override
+	public void run() {
+		WorkspaceTree workspaceTree = UICommonToolkit.getWorkspaceManager().getWorkspaceTree();
+		TreePath selectedPath = workspaceTree.getSelectionPath();
+		try {
+			if (selectedPath != null && selectedPath.getLastPathComponent() instanceof DefaultMutableTreeNode) {
+				DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) selectedPath.getLastPathComponent();
+				TreeNodeData nodeData = (TreeNodeData) selectedNode.getUserObject();
+				if (nodeData.getType() == NodeDataType.DATASOURCE) {
+					Datasource tempDatasource = (Datasource) nodeData.getData();
+					ClipBoardUtilties.setSysClipboardText(tempDatasource.getConnectionInfo().getServer());
+				}
+				Application.getActiveApplication().getOutput().output(DataViewProperties.getString("String_CopyFilePath"));
+			}
+		} catch (Exception e) {
+			Application.getActiveApplication().getOutput().output(e);
+		}
+	}
+
+}
