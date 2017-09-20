21d3483268a3998d8cb0e41a4692497a92bd7ced
yuanR <yuanrui@supermap.com>
2017-09-20 21:48:50 +0800

    字段转文本缺陷      yuanR

--- WorkflowView/src/main/java/com/supermap/desktop/WorkflowView/meta/metaProcessImplements/typeConversion/MetaProcessFieldToText.java
+++ WorkflowView/src/main/java/com/supermap/desktop/WorkflowView/meta/metaProcessImplements/typeConversion/MetaProcessFieldToText.java
@@ -36,7 +36,6 @@ public class MetaProcessFieldToText extends MetaProcessTypeConversion {
 		inputDataset = new ParameterSingleDataset(DatasetType.POINT, DatasetType.LINE, DatasetType.REGION, DatasetType.TEXT, DatasetType.POINT3D, DatasetType.LINE3D, DatasetType.REGION3D, DatasetType.MODEL);
 		outputData = new ParameterSaveDataset();
 		fieldComboBox = new ParameterFieldComboBox(ProcessProperties.getString("String_ExportField"));
-		fieldComboBox.setRequisite(true);
 
 		Dataset dataset = DatasetUtilities.getDefaultDataset(DatasetType.POINT, DatasetType.LINE, DatasetType.REGION, DatasetType.TEXT, DatasetType.POINT3D, DatasetType.LINE3D, DatasetType.REGION3D, DatasetType.MODEL);
 		if (dataset != null) {
@@ -100,7 +99,14 @@ public class MetaProcessFieldToText extends MetaProcessTypeConversion {
 			for (int i = 0; i < src.getFieldInfos().getCount(); i++) {
 				FieldInfo fieldInfo = src.getFieldInfos().get(i);
 				if (!fieldInfo.isSystemField() && !fieldInfo.getName().toLowerCase().equals("smuserid")) {
-					resultDataset.getFieldInfos().add(fieldInfo);
+					try {
+						resultDataset.getFieldInfos().add(fieldInfo);
+					} catch (Exception e) {
+						Application.getActiveApplication().getOutput().output(e.getMessage());
+						e.printStackTrace();
+						outputData.getResultDatasource().getDatasets().delete(resultDataset.getName());
+						return false;
+					}
 				}
 			}
 			recordsetResult = resultDataset.getRecordset(false, CursorType.DYNAMIC);
