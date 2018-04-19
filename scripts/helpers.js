c34925a1e13a263acc2abdd353fed766a55bad29
qinlj <qinlijuan@supermap.com>
2018-04-19 09:42:28 +0800

    增加入门、工具箱选项卡

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -6,7 +6,7 @@ var url = require('url');
 var cheerio = require('cheerio');
 var lunr = require('lunr');
 
-var localizedPath = ['docs', 'api'];
+var localizedPath = ['docs', 'getstarted'];
 
 function startsWith(str, start){
   return str.substring(0, start.length) === start;
@@ -170,7 +170,7 @@ hexo.extend.helper.register('lunr_index', function(data){
 hexo.extend.helper.register('canonical_path_for_nav', function(){
   var path = this.page.canonical_path;
 
-  if (startsWith(path, 'docs/') || startsWith(path, 'api/')){
+  if (startsWith(path, 'docs/') || startsWith(path, 'getstarted/')){
     return path;
   } else {
     return '';
---
+++ source/en/tools/index.md
@@ -0,0 +1,17 @@
+---
+title: 工具箱
+---
+
+### 使用说明
+
+　　工具箱集成了丰富的数据处理和数据分析功能。
+
+
+
+### 相关主题
+
+![](img/smalltitle.png) [关于缓冲区分析](BufferTheory.html)
+
+![](img/smalltitle.png) [缓冲区分析应用实例](BufferAnalyst_Example.html)
+
+
